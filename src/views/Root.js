fffd82bff1ab0afe53fa4a3b92841219116aea7e
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-10-19 21:31:00 +0200

    add Theme Provider, Normaliza& GlobalStyle to Root

--- src/views/Root.js
+++ src/views/Root.js
@@ -1,8 +1,16 @@
+import { ThemeProvider } from 'styled-components';
+import { theme } from 'theme/theme';
+import { Normalize } from 'styled-normalize';
+import { GlobalStyle } from 'theme/GlobalStyle';
+
 function Root() {
   return (
-    <div>
-      <p>Hello Fucking World</p>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Normalize />
+      <GlobalStyle />
+      <div>
+      </div>
+    </ThemeProvider>
   );
 }
 
