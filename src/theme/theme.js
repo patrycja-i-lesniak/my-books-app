2b5a57f6b186cb20f3cc3db07b05e18aa0117eb7
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-10-19 21:29:41 +0200

    add items to theme

---
+++ src/theme/theme.js
@@ -0,0 +1,31 @@
+export const theme = {
+  colors: {
+    white: '#FFFFFF',
+    lightLipstick: '#C4024F',
+    lipstick: '#AE0046',
+    gray: '#808080',
+    black: '#000000',
+  },
+
+  font: {
+    light: 300,
+    regular: 400,
+    bold: 600,
+    xxs: '1rem',
+    xs: '1.2rem',
+    s: '1.6rem',
+    m: '2.1rem',
+    l: '2.4rem',
+    xl: '4rem',
+  },
+
+  breakpoints: {
+    small: 598,
+    medium: 768,
+  },
+shadows: {
+  boxShadow: '0 5px 20px -5px rgba(128, 128, 128, 0.8)',
+  textShadow: "-1px 3px 3px #bfbfbf",
+}
+  
+};
