07bc9d30f35331e73f2b8a0e8071fba370dd9465
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-10-20 19:14:22 +0200

    add imports to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,11 +19,13 @@ module.exports = {
   },
   plugins: ['react', 'import'],
   rules: {
-    'import/no-unresolved': [2, {commonjs: true, amd: true}],
+    'import/named': 2,
+    'import/namespace': 2,
+    'import/default': 2,
+    'import/export': 2,
     // suppress errors for missing 'import React' in files
     'react/react-in-jsx-scope': 'off',
     // allow jsx syntax in js files (for next.js project)
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }], //should add ".ts" if typescript project,
-    
   },
 };
