39b99ba8ba628d1b00fb362431187ce6a19c316d
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-10-21 16:32:59 +0200

    change weight of paragraphs font

--- src/components/atoms/Paragraph/Paragraph.js
+++ src/components/atoms/Paragraph/Paragraph.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const Paragraph = styled.p`
   font-size: ${({ theme }) => theme.fontSize.xs};
-  font-weight: ${({ theme }) => theme.light};
+  font-weight: ${({ theme }) => theme.regular};
 `;
 
 export default Paragraph;
