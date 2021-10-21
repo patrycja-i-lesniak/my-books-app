81509bfce03b85f07d6148d36f1c4821eada1fcf
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-10-21 17:51:45 +0200

    reset margin in Input and style placeholder

--- src/components/atoms/Input/Input.js
+++ src/components/atoms/Input/Input.js
@@ -2,14 +2,18 @@ import styled from "styled-components";
 
 const Input = styled.input`
   position: relative;
-  margin: 8px 0;
+  margin: 0;
   width: 100%;
   color: ${({ theme }) => theme.colors.lipstick};
   font-size: ${({ theme }) => theme.font.xxs};
   border: none;
   transition: 0.1s all;
-  border-bottom: 1px solid ${({ theme }) => theme.colors.silver}; 
+  border-bottom: 1px solid ${({ theme }) => theme.colors.silver};
   outline: none;
+
+  ::placeholder {
+    color: ${({ theme }) => theme.colors.gray};
+  }
 `;
 
 export default Input;
