39d5317f764fd5f601ccab8c34bd3f57ddacc03f
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-10-21 16:31:57 +0200

    restyle button

--- src/components/atoms/Button/Button.js
+++ src/components/atoms/Button/Button.js
@@ -5,10 +5,14 @@ const Button = styled.button`
   color: ${({ theme }) => theme.colors.white};
   background: ${({ theme }) => theme.colors.lipstick};
   font-weight: 600;
-  padding: 8px 12px;
+  width: 120px;
+  height: 36px;
+  font-size: ${({ theme }) => theme.font.xxs};
   border-radius: 3px;
   cursor: pointer;
   box-shadow: ${({ theme }) => theme.shadows.boxShadow};
+  padding: 0;
+  margin: 0;
 
   &:active {
     border-color: ${({ theme }) => theme.colors.lightLipstick};
@@ -23,6 +27,22 @@ const Button = styled.button`
     css`
       background-color: ${({ theme }) => theme.colors.white};
       color: ${({ theme }) => theme.colors.lipstick};
+    `}
+
+  ${({ books }) =>
+    books &&
+    css`
+      border: none;
+      box-shadow: none;
+      background-color: transparent;
+      color: ${({ theme }) => theme.colors.gray};
+      font-size: ${({ theme }) => theme.font.xxs};
+      font-weight: ${({ theme }) => theme.font.light};
+
+      &:active {
+        font-weight: ${({ theme }) => theme.font.bold};
+        color: ${({ theme }) => theme.colors.white};
+      }
     `}
 `;
 
