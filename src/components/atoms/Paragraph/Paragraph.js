04f0842a4809d2236e198b453f31b7fde9039077
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-10-29 17:06:10 +0200

    change paths to theme

--- src/components/atoms/Paragraph/Paragraph.js
+++ src/components/atoms/Paragraph/Paragraph.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 const Paragraph = styled.p`
-  font-size: ${({ theme }) => theme.fontSize.xs};
-  font-weight: ${({ theme }) => theme.regular};
+  font-size: ${({ theme }) => theme.font.xs};
+  font-weight: ${({ theme }) => theme.font.regular};
 `;
 
 export default Paragraph;
