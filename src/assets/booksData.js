30254b561224904dff5c65c74412092596034bf1
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-10-29 17:05:35 +0200

    add example data

---
+++ src/assets/booksData.js
@@ -0,0 +1,105 @@
+import malezycie from "assets/images/malezycie.jpg";
+import ksiegatesknot from "assets/images/ksiegatesknot.jpg";
+import dziesiecminut from "assets/images/10minut.jpg";
+import malarzdusz from "assets/images/malarzdusz.jpg";
+import wyznaje from "assets/images/wyznaje.jpg";
+
+export const booksData = [
+  {
+    imageUrl: malezycie,
+    title: "Małe życie",
+    author: "Hanya Yanagihara",
+    // series: "",
+    // date: "2018-10-17",
+    // isbn: 9788328037342,
+    // translation: "Jolanta Kozak",
+    // publishing: "W.A.B",
+    // pages: 817,
+    // description: "Zachwycająca powieść o przyjaźni poddanej ogromnej próbie. Oto historia życia czterech przyjaciół – o ich niełatwym dojrzewaniu, ogromnych sukcesach, bolesnych wyborach i tyranii pamięci, od której nie sposób czasem się uwolnić… To również opowieść o jednym z najbardziej fascynujących miast świata – Nowym Jorku – który jest piątym, równorzędnym bohaterem tej olśniewającej książki.",
+    // goodReadsLink: "https://www.goodreads.com/book/show/29920252-ma-e-ycie"
+  },
+
+  {
+    imageUrl: ksiegatesknot,
+    title: "Księga tęsknot",
+    author: "Sue Monk Kidd",
+    // series: "",
+    // date: "2021-06-30",
+    // ISBN: 9788308074015,
+    // translation: "Łukasz Małecki",
+    // publishing: "Wydawnictwo Literackie",
+    // pages: 635,
+    // description: "Długo wyczekiwana nowa książka autorki światowych hitów – Sekretne życie pszczół i Czarne skrzydła, które przez wiele tygodni zajmowały pierwsze miejsca listy bestsellerów „New York Timesa”. Na imię mi Ana. Byłam żoną Jezusa z Nazaretu, syna Józefa. Nazywałam go Ukochanym, a on mnie Małym Grzmotem, zawsze się przy tym śmiejąc. Twierdził, iż kiedy spałam, rozlegało się we mnie dudnienie, niczym dźwięk pioruna dolatujący z dalekiej doliny Nahal Zippori lub jeszcze odleglejszej Jordanii. Nie wątpię, że naprawdę coś słyszał. Odkąd sięgam pamięcią, gnieździły się we mnie tęsknoty, które wzbierały jak nokturny, mącące swymi dźwiękami ciszę nocy. Tak rozpoczyna się opowieść Any. Młodej Żydówki z zamożnej rodziny, blisko związanej z dworem tetrarchy Heroda Antypasa. Jej los dyktują mężczyźni i tradycje, a przekleństwem nastolatki jest pęd do wiedzy, który nie pozwala się jej podporządkować. Przemożną chęć do wyrażania swoich myśli i spisywania losu kobiet pielęgnuje w dziewczynie ciotka Yaltha, uczona siostra jej ojca, która po latach banicji powróciła w tajemnicy na łono rodziny. Brutalny męski świat zmusza Anę do ucieczki. Dziewczyna rozpoczyna własną podróż przez życie, spisując pomijaną w oficjalnych przekazach historię kobiet początku naszej ery. Jej droga rozpoczyna się w 16 roku w Seforis, a kończy w 60 roku w Egipcie. Księga tęsknot przedstawia, z kobiecej perspektywy, wydarzenia znane z Nowego Testamentu. To poruszająca, hipnotyzująca powieść, napisana z rozmachem i jednocześnie szacunkiem do religii chrześcijańskiej, ukazująca ludzką twarz Jezusa z Nazaretu i jego życie z Aną.",
+    // goodReadsLink: "https://www.goodreads.com/book/show/58015325-ksi-ga-t-sknot?from_search=true&from_srp=true&qid=Lm8KBRgkfU&rank=1#",
+  },
+
+  {
+    imageUrl: dziesiecminut,
+    title: "10 minut i 38 sekund na tym dziwnym świecie",
+    author: "Elif Shafak",
+    // series: "",
+    // date: "2020-10-14",
+    // ISBN: 9788366553552,
+    // translation: "Natalia Wiśniewska",
+    // publishing: "Wydawnictwo Poznańskie",
+    // pages: 456,
+    // description: "Czy wystarczy 10 minut i 38 sekund, aby opowiedzieć całe ludzkie życie? Leila nie żyje. Jej ciało jest martwe, ale mózg jeszcze pracuje. Każda kolejna minuta przynosi ze sobą wspomnienie innych wydarzeń, smaków i zapachów. Obrazy pozornie wyrwane z kontekstu łączą się w zmysłową opowieść o życiu w świecie, w którym wielu ludziom nadal odbiera się prawo do bycia sobą. Elif Shafak jest jedną z najważniejszych współczesnych pisarek, a 10 minut i 38 sekund na tym dziwnym świecie zostało uznane za jej najlepszą powieść i znalazło się na krótkiej liście tytułów nominowanych do Nagrody Bookera.",
+    // goodReadsLink: "https://www.goodreads.com/book/show/53454220-10-minut-i-38-sekund-na-tym-dziwnym-wiecie?ac=1&from_search=true&qid=FyvCVz5VE8&rank=1#",
+  },
+
+  {
+    imageUrl: malarzdusz,
+    title: "Malarz dusz",
+    author: "Ildefonso Falcones",
+    // series: "",
+    // date: "2020-05-20",
+    // ISBN: 9788381259613,
+    // translation: "Joanna Ostrowska, Elżbieta Rzewuska",
+    // publishing: "Albatros",
+    // pages: 704,
+    // description: "Sztuka, miłość, pasja i walka o ideały! Ildefonso Falcones, autor niezapomnianej „Katedry w Barcelonie”, wskrzesza ekscytujące czasy wielkich przemian społecznych i buntu wobec odwiecznych tradycji i zakurzonych konwenansów.Barcelona rok 1901. Przez miasto przetacza się fala niepokojów społecznych. Ubóstwo najbiedniejszych dzielnic kontrastuje z bogactwem dużych eleganckich alei, wzdłuż których wyrosły już niezwykłe budynki, symbol nadejścia epoki modernizmu.Dalmau Sala, syn skazanych na śmierć anarchistów, żyje na granicy dwóch światów. Z jednej strony członkowie jego rodziny oraz Emma, kobieta, którą kocha, są zagorzałymi zwolennikami walki o prawa robotników. Z drugiej, zatrudnienie w pracowni ceramiki don Manuela Bello, jego mentora i obrońcy wiary katolickiej, pozwala mu obracać się w towarzystwie, w którym kwitnie sztuka i dobrobyt. Artyście nie będzie łatwo odnaleźć właściwą ścieżkę, którą powinien podążyć, aby zrozumieć, że tym, co się w życiu liczy, są jego przekonania dotyczące równości i sprawiedliwości społecznej, jego czysta dusza i miłość odważnej i silnej kobiety. A przede wszystkim liczy się jego sztuka – obrazy, na których uwiecznia ubóstwo swojego miasta, w którym tli się już iskierka buntu i rewolty.",
+    // goodReadsLink: "https://www.goodreads.com/book/show/17407389-wyznaj?ac=1&from_search=true&qid=NL0yNPL9lu&rank=1",
+  },
+
+  {
+    imageUrl: wyznaje,
+    title: "Wyznaję",
+    author: "Jaume Cabré",
+    // series: "",
+    // date: "2013-02-20",
+    // ISBN: 9788363656249,
+    // translation: "Anna Sawicka",
+    // publishing: "Marginesy",
+    // pages: 768,
+    // description: "„Wielka powieść europejska”, „przełom w literaturze”, „powieść-katedra o idealnych proporcjach i epickim, pełnym kunsztownych detali wykonaniu”, „książka, która przeżyje nas wszystkich”.To trzymające w napięciu do ostatniej strony wyznanie miłosne i spowiedź człowieka starającego się przeniknąć istotę zła – w świecie i sobie samym. Historia chłopca dorastającego samotnie wśród książek, który musi zmierzyć się z rodzinnymi tajemnicami. Za sprawą osiemnastowiecznych skrzypiec zagłębia się w mroki dziejów hiszpańskiej inkwizycji i piekła dwudziestowiecznej Europy. wyznajCabre, którego pasją jest muzyka, skomponował tę powieść jak symfonię, z częstymi zmianami nastroju, tempa i głosu narracji. Bogactwo wątków, postaci, pomysłów literackich oszałamia i wzbudza zachwyt..",
+    // goodReadsLink: "https://www.goodreads.com/book/show/17407389-wyznaj?ac=1&from_search=true&qid=NL0yNPL9lu&rank=1",
+  },
+];
+
+export const authorsData = [
+  {
+    imageUrl: "https://s.lubimyczytac.pl/upload/authors/30385/876459-352x500.jpg",
+    author: "Brandon Sanderson",
+    date: "19.12.1975",
+    description:
+      "Amerykański pisarz fantastyki i powieści przygodowych dla młodzieży, wykładowca twórczego pisania. Dwukrotny laureat nagrody Hugo. Jego książka „Stalowe serce” została nominowana w Plebiscycie Książka Roku 2015 lubimyczytac.pl w kategorii SF. Sanderson konsekwentnie pisał przez cały okres studiów licencjackich i magisterskich, a do 2003 roku napisał dwanaście powieści, chociaż żaden z wydawców nie zaakceptował żadnej z nich do publikacji. Będąc w trakcie studiów podyplomowych na BYU, redaktor Tor Books Moshe Feder skontaktował się z nim, aby powiedzieć, że chce nabyć jedną ze swoich książek. Sanderson przedłożył rękopis swojej szóstej powieści Elantris półtora roku wcześniej. Elantris został opublikowany przez Tor Books w dniu 21 kwietnia 2005 r., Aby ogólnie pozytywne recenzje. Następnie w 2006 r. Ukazał się Mistborn: The Final Empire , pierwsza książka w jego Mistbornfantastyczna trylogia, w której „allomanci” - ludzie zdolni do „spalania” metali i stopów po ich spożyciu - zyskują wzmocnione zmysły i kontrolę nad potężnymi siłami nadprzyrodzonymi. W 2007 roku opublikował drugą książkę z serii Mistborn The Well of Ascension . Później tego samego roku Sanderson opublikował powieść dla dzieci Alcatraz Versus the Evil Librarians , o chłopcu o imieniu Alcatraz z talentem do robienia rzeczy. Alcatraz konfrontuje się z grupą złych bibliotekarzy, którzy chcą przejąć władzę nad światem. Pierwsze z jego „ praw magii ” opublikowano po raz pierwszy w 2007 r., A drugie i trzecie odpowiednio w 2012 i 2013 r. W 2008 r. Opublikowano trzecią i ostatnią książkę trylogii Mistborn, zatytułowaną The Hero of Ages , a także drugą książkę z serii Alcatraz, zatytułowanąAlcatraz kontra kości skryba . W tym samym roku rozpoczął podcast Writing Excuses z Howardem Taylerem i Danem Wellsem. W 2009 roku Tor Books opublikował Warbreaker , który pierwotnie pojawił się seryjnie na stronie Sandersona, gdy pisał powieść od 2006 do 2009 roku. W tym samym roku ukazała się trzecia książka o Alcatraz , zatytułowana Alcatraz kontra rycerze kryształów .",
+    oficialPage: "http://www.brandonsanderson.com/",
+  },
+  {
+    imageUrl: "https://s.lubimyczytac.pl/upload/authors/81732/766091-352x500.jpg",
+    author: "Samantha Shannon",
+    date: "08.11.1991",
+    description:
+      "Urodziła się w zachodnim Londynie w 1991 roku. Zaczęła pisać w wieku piętnastu lat. W latach 2010–2013 studiowała angielską literaturę i literaturę powszechną w college’u Świętej Anny w Oxfordzie. W 2013 roku wydała Czas Żniw, pierwszą książkę z siedmiotomowej serii. Czas Żniw stał się bestsellerem w rankingu „New York Timesa”, „Sunday Timesa” i „Asian Age”, a także został wybrany Książką Roku przez „Daily Mail”, „Stylist” i „Huffington Post”. Ponadto powieść nazwano jedną z najlepszych książek roku 2013 w serwisie Amazon. Czas Żniw został przetłumaczony na dwadzieścia osiem języków. Prawa do ekranizacji nabyła wytwórnia Imaginarium Studios i 20th Century Fox. W 2014 roku Samantha Shannon znalazła się na liście Evening Standard Power 1000.",
+    oficialPage: "http://samantha-shannon.blogspot.com/",
+  },
+];
+
+export const notesData = [
+  {
+    title: "Lorem ipsum",
+    date: "21.10.2021",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+];
