338662d6890bb954a467f250ac83856ec689bde4
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-10-30 21:31:03 +0200

    styled components on hover, restyle Card

--- src/components/molecules/Card/Card.js
+++ src/components/molecules/Card/Card.js
@@ -1,6 +1,7 @@
-import PropTypes from "prop-types";
-import styled from "styled-components";
-import Button from "components/atoms/Button/Button";
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import Button from 'components/atoms/Button/Button';
+import Paragraph from 'components/atoms/Paragraph/Paragraph';
 
 const StyledWrapper = styled.div`
   display: flex;
@@ -17,11 +18,10 @@ const StyledWrapper = styled.div`
   &:hover {
     transform: scale(1.03);
     background-color: ${({ theme }) => theme.colors.lipstick};
-    color: ${({ theme }) => theme.colors.white};
   }
 `;
 
-const StyledImage = styled.img` 
+const StyledImage = styled.img`
   height: 225px;
   padding: 0 0 10px;
   background-color: transparent;
@@ -34,7 +34,12 @@ const StyledTitle = styled.h1`
   text-align: center;
   padding: 0 0 10px;
   margin: 0;
+
+  ${StyledWrapper}:hover & {
+    color: white;
+  }
 `;
+
 const StyledAuthor = styled.h2`
   color: ${({ theme }) => theme.colors.black};
   font-size: ${({ theme }) => theme.font.xxs};
@@ -42,30 +47,67 @@ const StyledAuthor = styled.h2`
   margin: 0;
   text-align: center;
   padding: 0 0 5px;
+
+  ${StyledWrapper}:hover & {
+    color: white;
+  }
 `;
 
-const Card = ({cardType, imageUrl, title, author}) => {
+const StyledParagraph = styled(Paragraph)`
+  font-size: 1rem;
+  color: ${({ theme }) => theme.colors.grey};
+`;
+
+const StyledButton = styled(Button)`
+  border: none;
+  box-shadow: none;
+  background-color: transparent;
+  color: ${({ theme }) => theme.colors.grey};
+  font-size: ${({ theme }) => theme.font.xxs};
+  font-weight: ${({ theme }) => theme.font.light};
+
+  ${StyledWrapper}:hover & {
+    color: white;
+  }
+
+  &:active {
+    font-weight: ${({ theme }) => theme.font.bold};
+    color: ${({ theme }) => theme.colors.white};
+  }
+`;
+
+const Card = ({ pageType, imageUrl, title, author, name, content, description, date }) => {
   return (
     <>
-      <StyledWrapper cardType={cardType}>
-        <StyledImage src={imageUrl}/>
-        <StyledTitle>{title}</StyledTitle>
+      <StyledWrapper pageType={pageType}>
+        <StyledImage pageType={pageType !== 'notes'} src={imageUrl} alt={title} />
+        <StyledTitle>
+          {title}
+          {name}
+        </StyledTitle>
+        <StyledParagraph>{date}</StyledParagraph>
         <StyledAuthor>{author}</StyledAuthor>
-        <Button books>see more</Button>
+        <Paragraph>{content}</Paragraph>
+        <Paragraph>{description}</Paragraph>
+        <StyledButton seeMore>see more</StyledButton>
       </StyledWrapper>
     </>
   );
 };
 
 Card.propTypes = {
-  cardType: PropTypes.oneOf(['small', 'big']),
+  pageType: PropTypes.oneOf(['home', 'books', 'authors', 'notes']),
   imageUrl: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
 };
 
 Card.defaultProps = {
-  cardTypes: 'medium',
+  pageTypes: 'books',
 };
 
 export default Card;
