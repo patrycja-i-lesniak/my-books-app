49d3f479b6c554ba24b6df0c88b4b2f828136d32
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-11-09 19:27:27 +0100

    add an icon to open a Modal

--- src/templates/GridTemplate.js
+++ src/templates/GridTemplate.js
@@ -1,7 +1,9 @@
-import styled, {css} from 'styled-components';
+import styled, { css } from 'styled-components';
 import PropTypes from 'prop-types';
 import UserPageTemplate from './UserPageTemplate';
 import Header from 'components/atoms/Header/Header';
+import ButtonIcon from 'components/atoms/ButtonIcon/ButtonIcon';
+import plusIcon from 'assets/icons/plus.svg';
 
 const StyledWrapper = styled.div`
   max-width: 1080px;
@@ -13,6 +15,7 @@ const StyledGrid = styled.div`
   grid-template-columns: repeat(5, 1fr);
   grid-gap: 40px 20px;
   justify-items: center;
+  position: relative;
 
   ${({ pageType }) =>
     pageType === 'home' &&
@@ -23,21 +26,30 @@ const StyledGrid = styled.div`
   ${({ pageType }) =>
     pageType === 'notes' &&
     css`
-      grid-template-columns: 1fr;
+      display: flex;
     `}
+`;
+
+const StyledButtonIcon = styled(ButtonIcon)`
+  border-radius: 50%;
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  z-index: 999;
 `;
 
 const GridTemplate = ({ children, pageType }) => (
   <UserPageTemplate>
     <StyledWrapper>
-      <Header >Nagłówek strony {pageType}</Header>
+      <Header>Nagłówek strony {pageType}</Header>
       <StyledGrid pageType={pageType}>{children}</StyledGrid>
+      <StyledButtonIcon icon={plusIcon} />
     </StyledWrapper>
   </UserPageTemplate>
 );
 
 GridTemplate.propTypes = {
-  children: PropTypes.string,
+  children: PropTypes.array,
   pageType: PropTypes.oneOf(['home', 'books', 'authors', 'notes']),
 };
 
