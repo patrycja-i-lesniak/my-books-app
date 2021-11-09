dc3b645924a61df8d779e771a81966cc67914e32
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-11-09 21:57:23 +0100

    rebuild Footer component

---
+++ src/components/atoms/Footer/index.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+import Paragraph from '../Paragraph/Paragraph';
+
+const StyledWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: ${({ theme }) => theme.colors.white};
+  background-color: ${({ theme }) => theme.colors.lipstick};
+  height: 5rem;
+  width: 100vw;
+  margin-top: 2rem;
+  position: fixed;
+  left: 0;
+  bottom: 0;
+`;
+
+const Footer = () => (
+  <StyledWrapper>
+    <Paragraph> © 2021 | Coded by Patrycja Leśniak</Paragraph>
+  </StyledWrapper>
+);
+
+export default Footer;
