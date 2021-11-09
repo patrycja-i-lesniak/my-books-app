f4ac76a791f9436d4f995a946f54e0ccf78585ce
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-11-09 19:24:54 +0100

    delete unnecessary styles, change size and url

--- src/components/atoms/ButtonIcon/ButtonIcon.js
+++ src/components/atoms/ButtonIcon/ButtonIcon.js
@@ -1,44 +1,17 @@
-import styled, {css} from 'styled-components';
-import hamburgerIcon from 'assets/icons/hamburger.svg';
-import closeIcon from 'assets/icons/close.svg';
+import styled from 'styled-components';
 
 const ButtonIcon = styled.button`
-  width: 36px;
-  height: 36px;
+  width: 48px;
+  height: 48px;
   border: 2px solid ${({ theme }) => theme.colors.lipstick};
   background: #ae0046;
   border-radius: 3px;
   cursor: pointer;
   box-shadow: ${({ theme }) => theme.shadows.boxShadow};
-  background-image: url(${hamburgerIcon});
   background-size: 60%;
   background-position: 50%;
   background-repeat: no-repeat;
-
-  &:active {
-    border-color: ${({ theme }) => theme.colors.lightLipstick};
-  }
-
-  @media (min-width: ${({ theme }) => theme.breakpoints.medium}px) {
-    display: none;
-  }
-
-  ${({ secondary }) =>
-    secondary &&
-    css`
-      background-image: url(${closeIcon});
-    `};
+  background-image: url(${({ icon }) => icon});
 `;
 
 export default ButtonIcon;
-
-// ${({ secondary }) =>
-//     secondary &&
-//     css`
-//       background-image: url(../../../assets/icons/close.svg);
-//     `};
-
-//     @media (max-width: ${({ theme }) => theme.breakpoints.medium}px) {
-//     background-image: url(../../../assets/icons/hamburger.svg);
-//     background-repeat: no-repeat;
-//   }
