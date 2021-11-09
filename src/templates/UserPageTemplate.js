ed57d25352df409627786c21fff5b3bf1d3efce5
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-11-10 00:17:43 +0100

    add Footer

--- src/templates/UserPageTemplate.js
+++ src/templates/UserPageTemplate.js
@@ -1,15 +1,18 @@
 import PropTypes from 'prop-types';
 import NavigationBar from 'components/organisms/NavigationBar/NavigationBar';
+import Footer from 'components/molecules/Footer';
+
 
 const UserPageTemplate = ({ children, pageType }) => (
   <>
     <NavigationBar pageType={pageType} />
     {children}
+    <Footer />
   </>
 );
 
 UserPageTemplate.propTypes = {
-  children: PropTypes.element,
+  children: PropTypes.object,
   pageType: PropTypes.oneOf(['home', 'books', 'authors', 'notes']),
 };
 
