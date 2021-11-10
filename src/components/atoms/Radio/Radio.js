131063627532e092381d960333e0f3d5fc04b93b
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-11-10 22:05:29 +0100

    create Radio component with 3 options

--- src/components/atoms/Radio/Radio.js
+++ src/components/atoms/Radio/Radio.js
@@ -1,41 +1,83 @@
+import React from 'react';
 import styled from 'styled-components';
-import PropTypes from "prop-types";
-import Input from 'components/atoms/Input/Input';
 
-const Label = styled.label`
+const StyledWrapper = styled.div`
+display: flex;
+justify-content: center;;
+`;
+
+const StyledLabel = styled.label`
   position: relative;
   display: inline-flex;
-  margin: 0 20px 50px;
+  margin: 0 30px 50px;
   align-items: center;
-  font-size: 0.8rem;
-  color: #808080;
+  font-size: 1.5rem;
+  color: ${({ theme }) => theme.colors.grey};
+  cursor: pointer;
 `;
 
-const StyledInput = styled(Input)`
-  opacity: 0;
+const StyledInput = styled.input`
+  /* opacity: 0; */
   position: absolute;
+  left: -30px;
+  cursor: pointer;
 `;
 
-const StyledRadioButton = styled.button`
-  margin-right: 5px;
-  width: 15px;
-  height: 15px;
-  border-radius: 50%;
-  border: 1px solid #808080;
-`;
+// const StyledRadioButton = styled.input`
+//   margin-right: 5px;
+//   width: 15px;
+//   height: 15px;
+//   border-radius: 50%;
+//   border: 1px solid ${({ theme }) => theme.colors.lipstick};
+//   background-color: ${({ theme }) => theme.colors.white};
+// `;
 
-const Radio = ({ id, checked, changeFn, children }) => (
-  <Label>
-    <StyledInput id={id} type="radio" checked={checked} onChange={changeFn} />
-    <StyledRadioButton>{children}</StyledRadioButton>
-  </Label>
-);
+const Radio = () => {
+  const [itemType, setItemType] = React.useState('book');
 
-Radio.propTypes = {
-    id: PropTypes.string.isRequired,
-    checked: PropTypes.bool.isRequired,
-    changeFn: PropTypes.func,
-    children: PropTypes.oneOf(['books', 'authors', 'notes']).isRequired,
+  const handleRadioButtonChange = (e) => {
+    setItemType(e.target.value);
+    console.log(e.target.value);
+  };
+
+  return (
+    <StyledWrapper>
+      <StyledLabel>
+        <StyledInput
+          type="radio"
+          value="book"
+          checked={itemType === 'book'}
+          onChange={handleRadioButtonChange}
+        />
+        book
+        {/* <StyledRadioButton/> */}
+        {/* {children} */}
+      </StyledLabel>
+      <StyledLabel>
+        <StyledInput
+          type="radio"
+          value="author"
+          checked={itemType === 'author'}
+          onChange={handleRadioButtonChange}
+        />
+        author
+        {/* <StyledRadioButton />
+        {children} */}
+      </StyledLabel>
+      <StyledLabel>
+        <StyledInput
+          type="radio"
+          value="note"
+          checked={itemType === 'note'}
+          onChange={handleRadioButtonChange}
+        />
+        note
+        {/* <StyledRadioButton />
+        {children} */}
+      </StyledLabel>
+    </StyledWrapper>
+  );
 };
 
 export default Radio;
+
