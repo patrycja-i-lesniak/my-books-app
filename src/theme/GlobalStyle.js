df1549fdacd59504edbe0ec325545dd5c1b11bd6
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-11-10 22:06:11 +0100

    add backdrop to GlobalStyle

--- src/theme/GlobalStyle.js
+++ src/theme/GlobalStyle.js
@@ -1,5 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 import { normalize } from 'styled-normalize';
+import styled from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
 ${normalize}
@@ -14,11 +15,21 @@ html  {
   font-size: 62.5%;
 }
 
-
 body {
     font-size: 1.6rem;
     margin: 0 auto;
     font-family: "Poppins", sans-serif;
     background-color: white;
 }
+`;
+
+export const StyledBackdrop = styled.div`
+  width: 100vw;
+  height: 100vh;
+  opacity: 60%;
+  background-color: black;
+  position: absolute;
+  left: 0;
+  top: 0;
+  z-index: 0;
 `;
