d82b04e3d8dc5314e4031d02d3fa5ec791e36b1e
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-11-10 10:13:11 +0100

    add a component with recently added items

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,10 +1,13 @@
 import GridTemplate from 'templates/GridTemplate';
 import RotaryBox from 'components/molecules/RotaryBox/RotaryBox';
+import LastAddedItems from 'components/molecules/LastAddedItems';
+
 
 const Home = () => (
   <>
     <GridTemplate pageType="home">
       <RotaryBox />
+      <LastAddedItems/>
     </GridTemplate>
   </>
 );
