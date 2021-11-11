05386f6112859268320c22bd31bec3554ce9357f
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-11-11 20:24:52 +0100

    delete plus button

--- src/components/molecules/Footer/index.js
+++ src/components/molecules/Footer/index.js
@@ -1,7 +1,5 @@
 import styled from 'styled-components';
 import Paragraph from '../../atoms/Paragraph/Paragraph';
-import ButtonIcon from 'components/atoms/ButtonIcon/ButtonIcon';
-import plusIcon from 'assets/icons/plus.svg';
 
 const StyledWrapper = styled.div`
   display: flex;
@@ -17,18 +15,9 @@ const StyledWrapper = styled.div`
   bottom: 0;
 `;
 
-const StyledButtonIcon = styled(ButtonIcon)`
-  border-radius: 50%;
-  position: fixed;
-  bottom: 70px;
-  right: 20px;
-  z-index: 999;
-`;
-
 const Footer = () => (
   <StyledWrapper>
     <Paragraph> © 2021 | Coded by Patrycja Leśniak</Paragraph>
-    <StyledButtonIcon icon={plusIcon} />
   </StyledWrapper>
 );
 
