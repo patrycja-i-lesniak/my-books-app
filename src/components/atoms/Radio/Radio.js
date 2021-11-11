955b326cc766d1b41e96cde5d9724f71a7a4f7a4
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-11-11 21:54:10 +0100

    rebuild Radio component using  Formik library

--- src/components/atoms/Radio/Radio.js
+++ src/components/atoms/Radio/Radio.js
@@ -1,13 +1,13 @@
-import React, {useState} from 'react';
+import React from 'react';
 import styled from 'styled-components';
+import { Formik, Field, Form } from 'formik';
 
 const StyledWrapper = styled.div`
-display: flex;
-justify-content: center;;
+  display: flex;
+  justify-content: center; ;
 `;
 
 const StyledLabel = styled.label`
-  position: relative;
   display: inline-flex;
   margin: 0 30px 50px;
   align-items: center;
@@ -16,67 +16,50 @@ const StyledLabel = styled.label`
   cursor: pointer;
 `;
 
-const StyledInput = styled.input`
-  /* opacity: 0; */
-  position: absolute;
-  left: -30px;
+const StyledField = styled(Field)`
+  margin-right: 5px;
+  width: 15px;
+  height: 15px;
+  border-radius: 50%;
+  border: 1px solid ${({ theme }) => theme.colors.lipstick};
+  background-color: ${({ theme }) => theme.colors.white};
   cursor: pointer;
 `;
 
-// const StyledRadioButton = styled.input`
-//   margin-right: 5px;
-//   width: 15px;
-//   height: 15px;
-//   border-radius: 50%;
-//   border: 1px solid ${({ theme }) => theme.colors.lipstick};
-//   background-color: ${({ theme }) => theme.colors.white};
-// `;
-
 const Radio = () => {
-  const [itemType, setItemType] = useState('book');
-
-  const handleRadioButtonChange = (e) => {
-    setItemType(e.target.value);
-  };
-
+  
   return (
     <StyledWrapper>
-      <StyledLabel>
-        <StyledInput
-          type="radio"
-          value="book"
-          checked={itemType === 'book'}
-          onChange={handleRadioButtonChange}
-        />
-        book
-        {/* <StyledRadioButton/> */}
-        {/* {children} */}
-      </StyledLabel>
-      <StyledLabel>
-        <StyledInput
-          type="radio"
-          value="author"
-          checked={itemType === 'author'}
-          onChange={handleRadioButtonChange}
-        />
-        author
-        {/* <StyledRadioButton />
-        {children} */}
-      </StyledLabel>
-      <StyledLabel>
-        <StyledInput
-          type="radio"
-          value="note"
-          checked={itemType === 'note'}
-          onChange={handleRadioButtonChange}
-        />
-        note
-        {/* <StyledRadioButton />
-        {children} */}
-      </StyledLabel>
+      <Formik
+        initialValues={{
+          value: '',
+        }}
+        onSubmit={async (values) => {
+          await new Promise((r) => setTimeout(r, 500));
+          alert(JSON.stringify(values, null, 3));
+        }}
+      >
+        {() => (
+          <Form>
+            <div role="group" aria-labelledby="my-radio-group">
+              <StyledLabel>
+                <StyledField type="radio" name="picked" value="books" />
+                book
+              </StyledLabel>
+              <StyledLabel>
+                <StyledField type="radio" name="picked" value="authors" />
+                author
+              </StyledLabel>
+              <StyledLabel>
+                <StyledField type="radio" name="picked" value="notes" />
+                note
+              </StyledLabel>
+            </div>
+          </Form>
+        )}
+      </Formik>
     </StyledWrapper>
   );
 };
 
 export default Radio;
-
