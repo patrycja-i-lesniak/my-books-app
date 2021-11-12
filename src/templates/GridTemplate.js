7696e7bc97d355fb8982c34bc255a8f8dc70051f
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-11-12 23:08:39 +0100

    move component from GridTemplate to MainTemplate

--- src/templates/GridTemplate.js
+++ src/templates/GridTemplate.js
@@ -1,13 +1,8 @@
-import React, { Component } from 'react';
 import styled, { css } from 'styled-components';
 import PropTypes from 'prop-types';
 import UserPageTemplate from './UserPageTemplate';
 import Header from 'components/atoms/Header/Header';
 import { Container } from 'components/atoms/Container';
-import ButtonIcon from 'components/atoms/ButtonIcon/ButtonIcon';
-import plusIcon from 'assets/icons/plus.svg';
-import Modal from 'components/organisms/Modal';
-import { StyledBackdrop } from 'theme/GlobalStyle';
 
 const StyledGrid = styled.div`
   display: grid;
@@ -30,52 +25,16 @@ const StyledGrid = styled.div`
     `}
 `;
 
-const StyledButtonIcon = styled(ButtonIcon)`
-  border-radius: 50%;
-  position: fixed;
-  bottom: 70px;
-  right: 20px;
-  z-index: 999;
-`;
-
-class GridTemplate extends Component {
-  state = {
-    isModalOpen: false,
-  };
-
-  openModal = () => {
-    this.setState(() => ({
-      isModalOpen: true,
-    }));
-  };
-
-  closeModal = () => {
-    this.setState(() => ({
-      isModalOpen: false,
-    }));
-  };
-
-  render() {
-    const { isModalOpen } = this.state;
-    const { children, pageType } = this.props;
-
-    return (
+const GridTemplate = ({ children, pageType }) => (
       <UserPageTemplate>
         <Container>
           <Header>Nagłówek strony {pageType}</Header>
-          <StyledGrid pageType={pageType}>{children}</StyledGrid>
-          <StyledButtonIcon icon={plusIcon} onClick={this.openModal} />
-          {isModalOpen && (
-            <>
-              <StyledBackdrop />
-              <Modal closeModalFn={this.closeModal} />
-            </>
-          )}
+          <StyledGrid pageType={pageType}>
+            {children}
+          </StyledGrid>
         </Container>
       </UserPageTemplate>
     );
-  }
-}
 
 GridTemplate.propTypes = {
   children: PropTypes.arrayOf(PropTypes.object).isRequired,
--- src/templates/MainTemplate.js
+++ src/templates/MainTemplate.js
@@ -1,19 +1,112 @@
+import styled, { ThemeProvider } from 'styled-components';
+import { Component } from 'react';
+import { withRouter } from 'react-router';
 import PropTypes from 'prop-types';
 import { theme } from 'theme/theme';
 import { GlobalStyle } from 'theme/GlobalStyle';
-import { ThemeProvider } from 'styled-components';
+import PageContext from 'context';
+import Modal from 'components/organisms/Modal';
+import { booksData, authorsData, notesData } from 'assets/booksData';
+import ButtonIcon from 'components/atoms/ButtonIcon/ButtonIcon';
+import plusIcon from 'assets/icons/plus.svg';
+import { StyledBackdrop } from 'theme/GlobalStyle';
 
-const MainTemplate = ({ children }) => (
-  <div>
-    <GlobalStyle />
-    <ThemeProvider theme={theme}>
-      {children}
-    </ThemeProvider>
-  </div>
-);
+const StyledButtonIcon = styled(ButtonIcon)`
+  border-radius: 50%;
+  position: fixed;
+  bottom: 70px;
+  right: 20px;
+  z-index: 999;
+`;
+
+const initialBookState = booksData;
+const initialAuthorState = authorsData;
+const initialNoteState = notesData;
+
+
+
+class MainTemplate extends Component {
+  state = {
+    pageType: 'home',
+    book: [...initialBookState],
+    author: [...initialAuthorState],
+    note: [...initialNoteState],
+    isModalOpen: false,
+  };
+
+  componentDidMount() {
+    this.setCurrentPage();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    this.setCurrentPage(prevState);
+  }
+
+  setCurrentPage = (prevState = '') => {
+    const pageTypes = ['home', 'books', 'authors', 'notes'];
+    const {
+      location: { pathname },
+    } = this.props;
+
+    const [currentPage] = pageTypes.filter((page) => pathname.includes(page));
+
+    if (prevState.pageType !== currentPage) {
+      this.setState({ pageType: currentPage });
+    }
+  };
+
+  addItem = (e, newItem) => {
+    e.preventDefault();
+
+    this.setState((prevState) => ({
+      [newItem.type]: [...prevState[newItem.type], newItem],
+    }));
+
+    this.closeModal();
+  };
+
+  openModal = () => {
+    this.setState(() => ({
+      isModalOpen: true,
+    }));
+  };
+
+  closeModal = () => {
+    this.setState(() => ({
+      isModalOpen: false,
+    }));
+  };
+
+  render() {
+    const { children } = this.props;
+    const { pageType } = this.state;
+    const { isModalOpen } = this.state;
+    const addItemFn = { ...this.state, addItem: this.state };
+
+    return (
+      <div>
+        <PageContext.Provider value={pageType}>
+          <GlobalStyle />
+          <ThemeProvider theme={theme}>{children}
+          <StyledButtonIcon icon={plusIcon} onClick={this.openModal} />
+          {isModalOpen && (
+            <>
+              <StyledBackdrop onClick={this.closeModal} />
+              <Modal closeModalFn={this.closeModal} addItemFn={addItemFn} />
+            </>
+          )}
+          </ThemeProvider>
+        </PageContext.Provider>
+      </div>
+    );
+  }
+}
 
 MainTemplate.propTypes = {
   children: PropTypes.element.isRequired,
+  location: PropTypes.shape({
+    pathname: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
-export default MainTemplate;
+export default withRouter(MainTemplate);
