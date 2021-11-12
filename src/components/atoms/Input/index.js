ceae414363382dd17a7ea80d130a3849ca36888d
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-11-12 20:31:57 +0100

    style Input component

---
+++ src/components/atoms/Input/index.js
@@ -0,0 +1,41 @@
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const StyledWrapper = styled.div`
+  /* position: relative; */
+  width: 100%;
+  display: flex; flex-direction: column;
+`;
+
+const StyledField = styled.input`
+  color: ${({ theme }) => theme.colors.lipstick};
+  font-size: 1.5rem;
+  border: none;
+  line-height: 2;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.silver};
+  outline: none;
+
+  ::placeholder {
+    color: ${({ theme }) => theme.colors.grey};
+    font-size: 1.3rem;
+  }
+`;
+
+const StyledLabel = styled.label`
+  font-size: 1.2rem;
+  color: ${({ theme }) => theme.colors.grey};
+`;
+
+const Input = ({name}) => (
+  <StyledWrapper>
+    <StyledField/>
+    <StyledLabel>{name}</StyledLabel>  
+</StyledWrapper>  
+);
+
+
+Input.propTypes = {
+    name: PropTypes.string.isRequired,
+};
+
+export default Input; 
