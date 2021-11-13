0d1fa03cb593f7b1877fde479a0bd427df24427e
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-11-13 23:36:46 +0100

    connect Notes with store

--- src/views/Notes.js
+++ src/views/Notes.js
@@ -1,14 +1,32 @@
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import GridTemplate from 'templates/GridTemplate';
 import Card from 'components/molecules/Card/Card';
-import { notesData } from 'assets/booksData';
 
-const notes = notesData;
-
-const Notes = () => (
+const Notes = ({notes}) => (
   <GridTemplate pageType="notes">
-    {notes.map(({ title, content, id, date }) => (
+    {notes.map(({ id, title, content, date }) => (
       <Card title={title} date={date} content={content} key={id} />
     ))}
   </GridTemplate>
 );
-export default Notes;
+
+Notes.propTypes = {
+  notes: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    imageUrl: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+  }),
+};
+
+Notes.defaultProps = {
+  notes: [],
+};
+
+const mapStateToProps = (state) => {
+  console.log(state);
+  const { notes } = state;
+  return { notes };
+};
+
+export default connect(mapStateToProps)(Notes);
