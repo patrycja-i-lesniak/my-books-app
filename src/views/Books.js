fdf93d9fbd21e61b4186ad4cc90dc89c7af71a96
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-11-13 23:36:33 +0100

    connect Books with store

--- src/views/Books.js
+++ src/views/Books.js
@@ -1,15 +1,35 @@
+/* eslint-disable react/prop-types */
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import GridTemplate from 'templates/GridTemplate';
 import Card from 'components/molecules/Card/Card';
-import { booksData } from 'assets/booksData';
 
-const books = booksData;
-
-const Books = () => (
+const Books = ({ books }) => (
   <GridTemplate pageType="books">
     {books.map(({ title, author, imageUrl, id }) => (
-      <Card imageUrl={imageUrl} title={title} author={author} key={id} />
+      <Card id={id} imageUrl={imageUrl} title={title} author={author} key={id} />
     ))}
   </GridTemplate>
 );
 
-export default Books;
+Books.propTypes = {
+  books: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    imageUrl: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+  }),
+};
+
+Books.defaultProps = {
+  books: [],
+};
+
+const mapStateToProps = state => {
+  console.log(state);
+  const {books} = state;
+  return {books};
+  
+};
+
+export default connect(mapStateToProps)(Books);
