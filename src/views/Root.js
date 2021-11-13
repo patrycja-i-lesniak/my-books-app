f6ac65e398ab3536c78ff591a57f16bf864c8c6e
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-11-13 18:52:20 +0100

    add routes to Root

--- src/views/Root.js
+++ src/views/Root.js
@@ -6,19 +6,20 @@ import Notes from 'views/Notes';
 import Books from 'views/Books';
 import Home from 'views/Home';
 import DetailsPage from 'views/DetailsPage';
+import { routes } from 'routes';
 
 const Root = () => (
     <HashRouter>
       <MainTemplate>
         <Switch>
           <Route exact path="/" render={() => <Redirect to="/home" />} />
-          <Route exact path="/home" component={Home} />
-          <Route exact path="/books" component={Books} />
-          <Route path="books/:id" component={DetailsPage} />
-          <Route exact path="/authors" component={Authors} />
-          <Route path="notes/:id" component={DetailsPage} />
-          <Route exact path="/notes" component={Notes} />
-          <Route path="notes/:id" component={DetailsPage} />
+          <Route exact path={routes.home} component={Home} />
+          <Route exact path={routes.books} component={Books} />
+          <Route path={routes.book} component={DetailsPage} />
+          <Route exact path={routes.authors} component={Authors} />
+          <Route path={routes.author} component={DetailsPage} />
+          <Route exact path={routes.notes}component={Notes} />
+          <Route path={routes.note} component={DetailsPage} />
         </Switch>
       </MainTemplate>
     </HashRouter>
