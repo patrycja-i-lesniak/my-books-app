db9ccb0e36e2503a4ed9aba11b0b5683f711996b
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-11-14 18:47:49 +0100

    change PropTypes, clear code

--- src/views/DetailsPage.js
+++ src/views/DetailsPage.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import DetailsPageTemplate from 'templates/DetailsPageTemplate';
 import { routes } from 'routes';
@@ -9,47 +9,52 @@ class DetailsPage extends Component {
   };
 
   componentDidMount() {
-    const {match} = this.props;
+    const { match } = this.props;
 
     switch (match.path) {
       case routes.book:
         this.setState({ pageType: 'books' });
         break;
-      case routes.note:
-        this.setState({ pageType: 'notes' });
-        break;
+
       case routes.author:
         this.setState({ pageType: 'authors' });
         break;
+
+      case routes.note:
+        this.setState({ pageType: 'notes' });
+        break;
+
       default:
         console.log('Something went wrong with matching paths');
     }
   }
 
   render() {
-    
-      const sampleNote = {
-        id: 1,
-        title: 'Title',
-        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, tempora quibusdam natus modi tempore esse adipisci, dolore odit animi',
-        imageUrl: 'http://image.url',
-      };
+    const exampleNote = {
+      id: 10,
+      title: 'Example note Title',
+      content:
+        'Lorem ipsum dolor sit amet. Aut consequatur pariatur ut odio ut doloribus vero a nihil cumque ut eligendi totam. Est dolor quasi a quos aliquid et deleniti explicabo? Ex sunt adipisci sit vero mollitia qui quam perspiciatis in suscipit illum et officiis illo quasi harum! Est quisquam quam vel dicta deleniti sed facere enim sit excepturi consectetur ad perferendis magni. Perferendis sapiente in sint placeat ut itaque quam. Vel eligendi iure sed sapiente assumenda a explicabo voluptatem quo quia possimus At autem officia. Ut unde odio eos quam dignissimos qui atque aliquam nam delectus quasi rem provident laudantium ex porro nisi. In consequatur provident ad doloremque aliquam est dolores maiores? Aut internos omnis vel minus enim et sapiente voluptatibus?',
+      imageUrl: 'http://image.url',
+    };
 
-const {pageType} = this.state;
+    const { pageType } = this.state;
 
-      return (
-        <DetailsPageTemplate
+    return (
+      <DetailsPageTemplate
         pageType={pageType}
-        title={sampleNote.title}
-        content={sampleNote.content}
-        imageUrl={sampleNote.imageUrl}
-    />
+        title={exampleNote.title}
+        content={exampleNote.content}
+        imageUrl={exampleNote.imageUrl}
+      />
     );
   }
 }
 
 DetailsPage.propTypes = {
-  match: PropTypes.string.isRequired,
+  match: PropTypes.shape({
+    path: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default DetailsPage;
