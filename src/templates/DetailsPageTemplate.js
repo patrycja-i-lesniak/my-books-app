9b915c6bd6a39891d604e78d2dc8d611d027e427
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-11-14 22:29:36 +0100

    add trash icon

--- src/templates/DetailsPageTemplate.js
+++ src/templates/DetailsPageTemplate.js
@@ -1,10 +1,12 @@
-import {Link} from "react-router-dom";
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import UserPageTemplate from "templates/UserPageTemplate"; 
-import Header from "components/atoms/Header/Header";
-import Paragraph from "components/atoms/Paragraph/Paragraph"; 
-import Button from "components/atoms/Button/Button";
+import UserPageTemplate from 'templates/UserPageTemplate';
+import Header from 'components/atoms/Header/Header';
+import Paragraph from 'components/atoms/Paragraph/Paragraph';
+import Button from 'components/atoms/Button/Button';
+import ButtonIcon from 'components/atoms/ButtonIcon/ButtonIcon';
+import trashIcon from 'assets/icons/trash.svg';
 
 const StyledWrapper = styled.div`
   display: flex;
@@ -20,21 +22,32 @@ const StyledWrapper = styled.div`
 `;
 
 const StyledParagraph = styled(Paragraph)`
-margin-bottom: 70px
+  margin-bottom: 70px;
 `;
+
 const StyledButton = styled(Button)`
-position: absolute;
-bottom: 30px;
-left: 30px;
+  position: absolute;
+  bottom: 30px;
+  left: 30px;
 `;
 
+const StyledButtonIcon = styled(ButtonIcon)`
+  border-radius: 50%;
+  position: absolute;
+  top: 30px;
+  right: 20px;
+  z-index: 999;
+`;
 
-const DetailsPageTemplate = ({pageType, title, content}) => (
+const DetailsPageTemplate = ({ pageType, title, content }) => (
   <UserPageTemplate pageType={pageType}>
     <StyledWrapper>
       <Header>{title}</Header>
       <StyledParagraph>{content}</StyledParagraph>
-    <StyledButton as={Link} to={`/${pageType}`}>go back</StyledButton>
+      <StyledButton as={Link} to={`/${pageType}`}>
+        go back
+      </StyledButton>
+      <StyledButtonIcon icon={trashIcon} />
     </StyledWrapper>
   </UserPageTemplate>
 );
@@ -46,3 +59,5 @@ DetailsPageTemplate.propTypes = {
 };
 
 export default DetailsPageTemplate;
+
+// dodać akcję usuwania notatki do StyledButtonIcon
