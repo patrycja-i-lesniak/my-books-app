5d2d2d810782e3ffc1c5c776f26f578648a70456
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-11-14 21:55:36 +0100

    style wrapper and paragraph

--- src/templates/DetailsPageTemplate.js
+++ src/templates/DetailsPageTemplate.js
@@ -7,9 +7,25 @@ import Paragraph from "components/atoms/Paragraph/Paragraph";
 import Button from "components/atoms/Button/Button";
 
 const StyledWrapper = styled.div`
-display: flex;
-flex-direction: column;
-padding: 0 25px;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  justify-content: center;
+  width: 90vw;
+  margin: 50px auto;
+  padding: 20px 50px;
+  box-shadow: ${({ theme }) => theme.shadows.boxShadow};
+  background-color: ${({ theme }) => theme.colors.white};
+  position: relative;
+`;
+
+const StyledParagraph = styled(Paragraph)`
+margin-bottom: 70px
+`;
+const StyledButton = styled(Button)`
+position: absolute;
+bottom: 30px;
+left: 30px;
 `;
 
 
@@ -17,8 +33,8 @@ const DetailsPageTemplate = ({pageType, title, content}) => (
   <UserPageTemplate pageType={pageType}>
     <StyledWrapper>
       <Header>{title}</Header>
-      <Paragraph>{content}</Paragraph>
-    <Button as={Link} to={`/${pageType}`}>go back</Button>
+      <StyledParagraph>{content}</StyledParagraph>
+    <StyledButton as={Link} to={`/${pageType}`}>go back</StyledButton>
     </StyledWrapper>
   </UserPageTemplate>
 );
