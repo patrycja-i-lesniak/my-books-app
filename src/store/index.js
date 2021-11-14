8b5f1251adc09ae57998986e0f733315c05045e7
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-11-14 18:49:40 +0100

    delete unnecessary line of code

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,8 @@
 import { createStore } from 'redux';
-import { rootReducer } from 'reducers';
-// import booksSlice from 'slice';
+import rootReducer from 'reducers';
 
 /* eslint-disable no-underscore-dangle */
 const store = createStore(
-  //   booksSlice /* preloadedState, */,
   rootReducer /* preloadedState, */,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
