e18c2f7a7cd7b6f840f545889a46eddbefc602ab
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-11-14 18:46:50 +0100

    pass cardType and id to the Books component

--- src/views/Books.js
+++ src/views/Books.js
@@ -1,4 +1,3 @@
-/* eslint-disable react/prop-types */
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import GridTemplate from 'templates/GridTemplate';
@@ -7,33 +6,30 @@ import Card from 'components/molecules/Card/Card';
 const Books = ({ books }) => (
   <GridTemplate pageType="books">
     {books.map(({ title, author, imageUrl, id }) => (
-      <Card id={id} imageUrl={imageUrl} title={title} author={author} key={id} />
+      <Card id={id} cardType='books' imageUrl={imageUrl} title={title} author={author} key={id} />
     ))}
   </GridTemplate>
 );
 
-
-
 Books.propTypes = {
   books: PropTypes.arrayOf(
-  PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    imageUrl: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    author: PropTypes.string.isRequired,
-  }),
-  )
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      imageUrl: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      author: PropTypes.string.isRequired,
+    }),
+  ),
 };
 
 Books.defaultProps = {
   books: [],
 };
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   console.log(state);
-  const {books} = state;
-  return {books};
-  
+  const { books } = state;
+  return { books };
 };
 
 export default connect(mapStateToProps)(Books);
