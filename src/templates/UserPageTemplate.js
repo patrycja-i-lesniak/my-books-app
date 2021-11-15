71887140b01c924940f2a64bcf143ab228057f68
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-11-15 20:41:48 +0100

    add MobileNavigation

--- src/templates/UserPageTemplate.js
+++ src/templates/UserPageTemplate.js
@@ -1,12 +1,20 @@
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import NavigationBar from 'components/organisms/NavigationBar/NavigationBar';
 import Footer from 'components/molecules/Footer';
+import MobileNavigation from 'components/organisms/MobileNavigation';
 
+const StyledMobileNavigation = styled(MobileNavigation)`
+  @media (max-width: ${({ theme }) => theme.breakpoints.medium}px) {
+    display: none;
+  }
+`;
 
 const UserPageTemplate = ({ children, pageType }) => (
   <>
     <NavigationBar pageType={pageType} />
     {children}
+    <StyledMobileNavigation />
     <Footer />
   </>
 );
