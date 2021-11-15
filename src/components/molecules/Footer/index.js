4e178acde3460c0089fbcf74fe31e3b9cdf1358a
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-11-15 20:40:54 +0100

    style footer on small screens

--- src/components/molecules/Footer/index.js
+++ src/components/molecules/Footer/index.js
@@ -13,11 +13,23 @@ const StyledWrapper = styled.div`
   position: fixed;
   left: 0;
   bottom: 0;
+  z-index: 9999;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.medium}px) {
+    height: 3rem;
+    margin: 0;
+  }
+`;
+
+const StyledParagraph = styled(Paragraph)`
+ @media (max-width: ${({ theme }) => theme.breakpoints.medium}px) {
+    font-size: 1rem;
+ }
 `;
 
 const Footer = () => (
   <StyledWrapper>
-    <Paragraph> © 2021 | Coded by Patrycja Leśniak</Paragraph>
+    <StyledParagraph> © 2021 | Coded by Patrycja Leśniak</StyledParagraph>
   </StyledWrapper>
 );
 
