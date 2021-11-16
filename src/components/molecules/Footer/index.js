3c794b466b43a3de83e6615fba9056e009be4d93
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-11-16 23:37:23 +0100

    expand the footer with a panel with contact links

--- src/components/molecules/Footer/index.js
+++ src/components/molecules/Footer/index.js
@@ -1,13 +1,16 @@
 import styled from 'styled-components';
 import Paragraph from '../../atoms/Paragraph/Paragraph';
+import { MdAlternateEmail } from 'react-icons/md';
+import { FaGithub } from 'react-icons/fa';
 
 const StyledWrapper = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   color: ${({ theme }) => theme.colors.white};
   background-color: ${({ theme }) => theme.colors.lipstick};
-  height: 5rem;
+  height: 6rem;
   width: 100vw;
   margin-top: 2rem;
   position: fixed;
@@ -22,14 +25,68 @@ const StyledWrapper = styled.div`
 `;
 
 const StyledParagraph = styled(Paragraph)`
- @media (max-width: ${({ theme }) => theme.breakpoints.medium}px) {
+  margin: 0;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.medium}px) {
     font-size: 1rem;
- }
+  }
 `;
+
+const Link = styled.a`
+  color: ${({ theme }) => theme.colors.white};
+  transition: color 0.3s;
+  margin: 0;
+
+  &:hover {
+    color: ${({ theme }) => theme.colors.black};
+  }
+`;
+
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  display: inline-flex;
+  gap: 10px;
+`;
+
+const Item = styled.li`
+  list-style: none;
+`;
+
+export const styleIcon = (Icon) => styled(Icon)`
+font-size: 2rem;
+`;
+
+export const contact = [
+  {
+    name: 'github',
+    url: 'https://github.com/patrycja-i-lesniak',
+    Icon: styleIcon(FaGithub),
+  },
+
+  {
+    name: 'email',
+    url: 'mailto: patrycja.lesniak@gmail.com',
+    Icon: styleIcon(MdAlternateEmail),
+  },
+];
 
 const Footer = () => (
   <StyledWrapper>
-    <StyledParagraph> © 2021 | Coded by Patrycja Leśniak</StyledParagraph>
+    <>
+      <StyledParagraph>© 2021 | Coded by Patrycja Leśniak</StyledParagraph>
+    </>
+    <>
+      <List>
+        {contact.map(({ name, url, Icon }) => (
+          <Item key={name}>
+            <Link href={url} title={name} target="_blank" rel="noreferrer">
+              <Icon />
+            </Link>
+          </Item>
+        ))}
+      </List>
+    </>
   </StyledWrapper>
 );
 
