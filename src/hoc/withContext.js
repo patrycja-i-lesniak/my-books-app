059521e3ee157c492d5b36459902703d014d2baf
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-11-16 18:51:31 +0100

    add HOC - withContext

---
+++ src/hoc/withContext.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PageContext from 'context'; 
+
+const withContext = (Component) => {
+    return function contextComponent(props)
+    {
+        return (
+            <PageContext.Consumer>
+            {context =>(
+                <Component {...props} pageContext={context}/>
+            )}
+            </PageContext.Consumer>
+        )
+    }
+};
+
+export default withContext;
