ed97f69b388d8161e6037367cb98f9458a2763df
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-11-17 19:29:34 +0100

    connect component with store, add select field

--- src/components/organisms/NewItemBar/index.js
+++ src/components/organisms/NewItemBar/index.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { Formik, Form } from 'formik';
 import Input from 'components/atoms/Input';
@@ -7,6 +7,7 @@ import Button from 'components/atoms/Button/Button';
 import Header from 'components/atoms/Header/Header';
 import withContext from 'hoc/withContext';
 import { addItemAction } from 'actions';
+import SelectStatus from 'components/atoms/SelectStatus';
 
 const StyledWrapper = styled.div`
   border-left: 3px solid ${({ theme }) => theme.colors.lipstick};
@@ -35,13 +36,20 @@ const StyledForm = styled(Form)`
 `;
 
 const StyledTextArea = styled(Input)`
-  height: 100px;
+  height: 120px;
   border: 1px solid ${({ theme }) => theme.colors.silver};
   line-height: 1.5;
-  border-radius: 10px;
+  border-radius: 5px;
   margin-top: 30px;
   resize: none;
   outline: none;
+  font-size: 1.5rem;
+  color: ${({ theme }) => theme.colors.lipstick};
+
+  ::placeholder {
+    color: ${({ theme }) => theme.colors.grey};
+    font-size: 1.2rem;
+  }
 `;
 
 const StyledButton = styled(Button)`
@@ -51,115 +59,144 @@ const StyledButton = styled(Button)`
   }
 `;
 
+
 const NewItemBar = ({ pageContext, isVisible, handleClose, addItem }) => {
-  
-  
   return (
- <StyledWrapper isVisible={isVisible}>
+    <StyledWrapper isVisible={isVisible}>
       <Header secondary>Add new {pageContext}</Header>
       <Formik
-      initialValues = {{
-      type: '',
-      title: '',
-      author: '',
-      imageUrl: '',
-      series: '',
-      date: '',
-      isbn: '',
-      translation: '',
-      publishing: '',
-      pages: '',
-      content: '',
-      LClink: '',
-    }}
-    onSubmit={values => {
-        addItem(pageContext, values);
-        handleClose();
-    }}
+        initialValues={{
+          type: '',
+          title: '',
+          author: '',
+          imageUrl: '',
+          series: '',
+          date: '',
+          isbn: '',
+          translation: '',
+          publishing: '',
+          pages: '',
+          content: '',
+          link: '',
+          status: '',
+        }}
+        onSubmit={(values) => {
+          addItem(pageContext, values);
+          handleClose();
+        }}
       >
-{({ values, handleChange, handleBlur }) => (
-
-            <StyledForm autoComplete="off" 
-            // onSubmit={handleSubmit, addItemFn }
-            >
-        <Input
-          type="text"
-          name="title"
-          onChange={handleChange}
-          onBlur={handleBlur}
-          value={values.title}
-        />
-        <Input
-          type="text"
-          name="author"
-          onChange={handleChange}
-          onBlur={handleBlur}
-          value={values.author}
-        />
-        <Input
-          type="url"
-          name="imageUrl"
-          onChange={handleChange}
-          onBlur={handleBlur}
-          value={values.imageUrl}
-        />
-        <Input
-          type="text"
-          name="series"
-          onChange={handleChange}
-          onBlur={handleBlur}
-          value={values.series}
-        />
-        <Input
-          type="date"
-          name="date"
-          onChange={handleChange}
-          onBlur={handleBlur}
-          value={values.date}
-        />
-        <Input
-          type="number"
-          name="isbn"
-          onChange={handleChange}
-          onBlur={handleBlur}
-          value={values.isbn}
-        />
-        <Input
-          type="text"
-          name="translation"
-          onChange={handleChange}
-          onBlur={handleBlur}
-          value={values.translation}
-        />
-        <Input
-          type="text"
-          name="publishing"
-          onChange={handleChange}
-          onBlur={handleBlur}
-          value={values.publishing}
-        />
-        <Input
-          type="number"
-          name="pages"
-          onChange={handleChange}
-          onBlur={handleBlur}
-          value={values.pages}
-          maxLenght="4"
-        />
-        <StyledTextArea
-          type="text"
-          name="content"
-          as="textarea"
-          onChange={handleChange}
-          onBlur={handleBlur}
-          value={values.content}
-        />
-        <StyledButton type="submit">Add new item</StyledButton>
-      </StyledForm> 
-  ) }
-  </Formik>
+        {({ values, handleChange, handleBlur }) => (
+          <StyledForm autoComplete="off">
+            {pageContext === 'authors' ? null : (
+              <Input
+                type="text"
+                name="title"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.title}
+              
+              />
+            )}
+            {pageContext === 'notes' ? null : (
+              <Input
+                type="text"
+                name="author"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.author}
+              />
+            )}
+            <Input
+              type="url"
+              name="imageUrl"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.imageUrl}
+              // placeholder="ex. https://image.jpg"
+            />
+            {pageContext === 'books' && (
+              <Input
+                type="text"
+                name="series"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.series}
+              />
+            )}
+            <Input
+              type="date"
+              name="date"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.date}
+            />
+            {pageContext === 'books' && (
+              <Input
+                type="number"
+                name="isbn"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.isbn}
+              />
+            )}
+            {pageContext === 'books' && (
+              <Input
+                type="text"
+                name="translation"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.translation}
+              />
+            )}
+            {pageContext === 'books' && (
+              <Input
+                type="text"
+                name="publishing"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.publishing}
+              />
+            )}
+            {pageContext === 'books' && (
+              <Input
+                type="number"
+                name="pages"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.pages}
+                maxLenght="4"
+              />
+            )}
+              <Input
+                type="url"
+                name="link"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.link}
+              />
+            {pageContext === 'books' && (
+              <SelectStatus
+                type="select"
+                name="status"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.status}
+              />
+            )}
+            <StyledTextArea
+              type="text"
+              name="content"
+              as="textarea"
+              placeholder="add description"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.content}
+            />
+            <StyledButton type="submit">Add new item</StyledButton>
+          </StyledForm>
+        )}
+      </Formik>
     </StyledWrapper>
-    
   );
 };
 
