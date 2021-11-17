8968f617aa54e93610755a40f62cd4d6ade626f9
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-11-17 16:33:00 +0100

    add conditional rendering of add item button

--- src/templates/MainTemplate.js
+++ src/templates/MainTemplate.js
@@ -1,26 +1,28 @@
-import styled, { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
+import styled from 'styled-components';
 import { Component } from 'react';
 import { withRouter } from 'react-router';
 import PropTypes from 'prop-types';
 import { theme } from 'theme/theme';
 import { GlobalStyle } from 'theme/GlobalStyle';
 import PageContext from 'context';
-import Modal from 'components/organisms/Modal';
+// import Modal from 'components/organisms/Modal';
 import { booksData, authorsData, notesData } from 'assets/booksData';
 import ButtonIcon from 'components/atoms/ButtonIcon/ButtonIcon';
 import plusIcon from 'assets/icons/plus.svg';
-import { StyledBackdrop } from 'theme/GlobalStyle';
+import NewItemBar from 'components/organisms/NewItemBar';
+// import { StyledBackdrop } from 'theme/GlobalStyle';
 
 const StyledButtonIcon = styled(ButtonIcon)`
-  border-radius: 50%;
-  position: fixed;
-  bottom: 70px;
-  right: 20px;
-  z-index: 999;
+border-radius: 50%;
+position: fixed;
+bottom: 70px;
+right: 20px;
+z-index: 999;
 
-  @media (max-width: ${({ theme }) => theme.breakpoints.medium}px) {
-    display: none;
-  }
+/* @media (max-width: ${({ theme }) => theme.breakpoints.medium}px) {
+  display: none;
+} */
 `;
 
 const initialBookState = booksData;
@@ -33,7 +35,8 @@ class MainTemplate extends Component {
     book: [...initialBookState],
     author: [...initialAuthorState],
     note: [...initialNoteState],
-    isModalOpen: false,
+    // isModalOpen: false,
+    isNewItemBarVisible: false,
   };
 
   componentDidMount() {
@@ -57,46 +60,56 @@ class MainTemplate extends Component {
     }
   };
 
-  addItem = (e, newItem) => {
-    e.preventDefault();
+  // addItem = (e, newItem) => {
+  //   e.preventDefault();
+  //   this.setState((prevState) => ({
+  //     [newItem.type]: [...prevState[newItem.type], newItem],
+  //   }));
+
+  //   this.closeModal();
+  // };
+
+  // openModal = () => {
+  //   this.setState(() => ({
+  //     isModalOpen: true,
+  //   }));
+  // };
+
+  // closeModal = () => {
+  //   this.setState(() => ({
+  //     isModalOpen: false,
+  //   }));
+  // };
+
+  toggleNewItemBar = () => {
     this.setState((prevState) => ({
-      [newItem.type]: [...prevState[newItem.type], newItem],
-      
-    }));
-
-    this.closeModal();
-  };
-
-  openModal = () => {
-    this.setState(() => ({
-      isModalOpen: true,
-    }));
-  };
-
-  closeModal = () => {
-    this.setState(() => ({
-      isModalOpen: false,
+      isNewItemBarVisible: !prevState.isNewItemBarVisible,
     }));
   };
 
   render() {
     const { children } = this.props;
     const { pageType } = this.state;
-    const { isModalOpen } = this.state;
-    const addItemFn = { ...this.state, addItem: this.state };
+    const { isNewItemBarVisible } = this.state;
+    // const { isModalOpen } = this.state;
+    // const addItemFn = { ...this.state, addItem: this.state };
 
     return (
       <div>
         <PageContext.Provider value={pageType}>
           <GlobalStyle />
-          <ThemeProvider theme={theme}>{children}
-          <StyledButtonIcon icon={plusIcon} onClick={this.openModal} />
-          {isModalOpen && (
+          <ThemeProvider theme={theme}>
+            {children}
+            {pageType === 'home' ? null : (
+              <StyledButtonIcon icon={plusIcon} onClick={this.toggleNewItemBar} />
+            )}
+            <NewItemBar handleClose={this.toggleNewItemBar} isVisible={isNewItemBarVisible} />
+            {/* {isModalOpen && (
             <>
               <StyledBackdrop onClick={this.closeModal} />
               <Modal closeModalFn={this.closeModal} addItemFn={addItemFn} />
-            </>
-          )}
+            </> */}
+            {/* )} */}
           </ThemeProvider>
         </PageContext.Provider>
       </div>
