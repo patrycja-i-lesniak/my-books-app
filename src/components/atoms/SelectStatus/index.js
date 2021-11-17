f374380ffed67a7421cc6e2456c7e815310d074e
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-11-17 20:49:51 +0100

    change colors in Select styles

--- src/components/atoms/SelectStatus/index.js
+++ src/components/atoms/SelectStatus/index.js
@@ -6,15 +6,16 @@ const Select = styled.select`
   margin-top: 3rem;
   border: 1px solid ${({ theme }) => theme.colors.silver};
   overflow: hidden;
-  color: ${({ theme }) => theme.colors.grey};
+  color: ${({ theme }) => theme.colors.lipstick};
   border-radius: 5px;
   padding: 10px 0;
   outline: none;
   font-size: 1.5rem;
+  border: 1px solid ${({theme})=> theme.colors.lipstick};
 `;
 
 const Option = styled.option`
-color: red;
+  
 `;
 
 class SelectStatus extends React.Component {
@@ -37,15 +38,14 @@ class SelectStatus extends React.Component {
 
   render() {
     return (
-        <Select value={this.state.value} onChange={this.handleChange}>
-          <Option value="">Select status</Option>
-          <Option value="read">read</Option>
-          <Option value="in progress">in progress</Option>
-          <Option value="to read">to read</Option>
-          <Option value="to buy">to buy</Option>
-          <Option value="borrowed">borrowed</Option>
-        </Select>
-       
+      <Select value={this.state.value} onChange={this.handleChange}>
+        <Option value="">Select status</Option>
+        <Option value="read">read</Option>
+        <Option value="in progress">in progress</Option>
+        <Option value="to read">to read</Option>
+        <Option value="to buy">to buy</Option>
+        <Option value="borrowed">borrowed</Option>
+      </Select>
     );
   }
 }
