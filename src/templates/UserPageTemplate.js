1f906ce61a03827609f72cf657ebc3d56336c0bb
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-11-18 21:18:26 +0100

    delete mobile navigation and move it to footer

--- src/templates/UserPageTemplate.js
+++ src/templates/UserPageTemplate.js
@@ -1,21 +1,12 @@
 import PropTypes from 'prop-types';
-// import styled from 'styled-components';
 import NavigationBar from 'components/organisms/NavigationBar/NavigationBar';
-import Footer from 'components/molecules/Footer';
+import Footer from 'components/molecules/FooterNavigation';
 import withContext from 'hoc/withContext';
-// import MobileNavigation from 'components/organisms/MobileNavigation';
-
-// const StyledMobileNavigation = styled(MobileNavigation)`
-//   @media (max-width: ${({ theme }) => theme.breakpoints.medium}px) {
-//     display: none;
-//   }
-// `;
 
 const UserPageTemplate = ({ children, pageContext}) => (
   <>
     <NavigationBar pageContext={pageContext}/>
     {children}
-    {/* <StyledMobileNavigation /> */}
     <Footer />
   </>
 );
