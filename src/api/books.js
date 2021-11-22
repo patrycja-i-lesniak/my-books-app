a53bf251b203517486cd6d3235aae0517a23a85c
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-11-22 22:34:51 +0100

    clean code and add console.log

--- src/api/books.js
+++ src/api/books.js
@@ -1,24 +1,23 @@
 import { axiosInstance, apiConfig } from 'api/config';
 
-
 export const fetchBooks = async () => {
   try {
-    const {data: dbData} = await axiosInstance.get(apiConfig.booksList);
+    const { data: dbData } = await axiosInstance.get(apiConfig.booksList);
 
-const records = [];
-dbData.records.forEach(book => {
-  records.push({
-    id: book.id,
-    title: book.fields.title,
-    imageUrl: book.fields.imageUrl,
-    author: book.fields.author,
-  });
-});
-console.log(records);
+    const records = [];
+    dbData.records.forEach(book => {
+      records.push({
+        id: book.id,
+        title: book.fields.title,
+        imageUrl: book.fields.imageUrl,
+        author: book.fields.author,
+      });
+    });
+    console.log(records);
 
-return records;
+    return records;
   } catch (e) {
     console.error(e);
-    console.log('Fucking bugs!!!!!')
+    console.log('Ups... something went wrong');
   }
-}
+};
