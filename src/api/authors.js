4c56d33e8af6254317a7fae377f546709c5c9ab9
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-11-22 22:18:45 +0100

    fetch authors data

---
+++ src/api/authors.js
@@ -0,0 +1,23 @@
+import { axiosInstance, apiConfig } from 'api/config';
+
+export const fetchAuthors = async () => {
+  try {
+    const { data: dbData } = await axiosInstance.get(apiConfig.authorsList);
+
+    const records = [];
+    dbData.records.forEach(author => {
+      records.push({
+        id: author.id,
+        title: author.fields.title,
+        imageUrl: author.fields.imageUrl,
+        author: author.fields.author,
+      });
+    });
+    console.log(records);
+
+    return records;
+  } catch (e) {
+    console.error(e);
+    console.log('Fucking bugs!!!!!');
+  }
+};
