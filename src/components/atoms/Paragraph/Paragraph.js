607830d5a73f29627559d2206af4b94cd9ca9c3d
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-11-23 16:53:02 +0100

    add theme to color in Paragraph

--- src/components/atoms/Paragraph/Paragraph.js
+++ src/components/atoms/Paragraph/Paragraph.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 const Paragraph = styled.p`
   font-size: 1.4rem;
   line-height: 1.5;
+  color: ${({ theme }) => theme.colors.black};
 `;
 
 export default Paragraph;
