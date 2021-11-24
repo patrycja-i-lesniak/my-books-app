cef66351deea107140f2f6ec4dd62a96fd8a2535
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-11-24 08:57:33 +0100

    add more records for books view

--- src/api/books.js
+++ src/api/books.js
@@ -10,7 +10,17 @@ export const fetchBooks = async () => {
         id: book.id,
         title: book.fields.title,
         imageUrl: book.fields.imageUrl,
-        author: book.fields.author,
+        firstName: book.fields.firstName,
+        lastName: book.fields.lastName,
+        status: book.fields.status,
+        content: book.fields.content,
+        numberOfPages: book.fields.numberOfPages,
+        date: book.fields.date,
+        translation: book.fields.translation,
+        publishing: book.fields.publishing,
+        isbn: book.fields.isbn,
+        series: book.fields.series,
+        LClink: book.fields.LClink,
       });
     });
 
@@ -18,5 +28,48 @@ export const fetchBooks = async () => {
   } catch (e) {
     console.error(e);
     console.log('Ups... something went wrong');
+  }
+};
+
+export const fetchBookDetails = async () => {
+  try {
+    const { data: dbData } = await axiosInstance.get(`${apiConfig.bookDetails}/${id}}`);
+
+    const {
+      id,
+      title,
+      firstName,
+      lastName,
+      imageUrl,
+      status,
+      content,
+      numberOfPages,
+      date,
+      translation,
+      publishing,
+      isbn,
+      series,
+      LClink,
+    } = dbData.fields;
+
+    return {
+      id,
+      title,
+      firstName,
+      lastName,
+      imageUrl,
+      status,
+      content,
+      numberOfPages,
+      date,
+      translation,
+      publishing,
+      isbn,
+      series,
+      LClink,
+    };
+  } catch (e) {
+    console.error(e);
+    console.log('kjdshkcdksjcjdskvckdsvkhedkjsvhjdfvjkdjvjkdfhkjvhdfjkvhjk');
   }
 };
