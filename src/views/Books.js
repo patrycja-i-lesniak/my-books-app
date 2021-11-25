0e433cedce3ca8ac41aa74d111bf8f6af6bdf912
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-11-25 17:16:27 +0100

    add hook with cleanup function and popup logic

--- src/views/Books.js
+++ src/views/Books.js
@@ -1,20 +1,61 @@
 import PropTypes from 'prop-types';
 import { useState, useEffect } from 'react';
 import GridTemplate from 'templates/GridTemplate';
-import Card from 'components/molecules/Card/Card';
-import { fetchBooks } from 'api/books';
+import Card from 'components/molecules/Card';
+import { base } from 'airtable/base';
+import Popup from 'components/molecules/Popup';
 
 const Books = () => {
   const [books, setBooks] = useState([]);
 
-  const fetchData = async () => {
-    const data = await fetchBooks();
-    console.log(data);
-    setBooks(data);
-  };
   useEffect(() => {
-    fetchData();
+    let isApiSubscribed = true;
+    base('books')
+      .select({ view: 'Grid view' })
+      .eachPage((records, fetchNextPage) => {
+        if (isApiSubscribed) {
+          setBooks(records);
+          fetchNextPage();
+          console.log(records);
+        }
+      });
+    return () => {
+      isApiSubscribed = false;
+    };
   }, []);
+
+  const [popup, setPopup] = useState({
+    show: false,
+    id: null,
+  });
+
+  const handleDelete = id => {
+    setPopup({
+      show: true,
+      id,
+    });
+    console.log('Open popup');
+  };
+
+  const handleDeleteTrue = books => {
+    if (popup.show && popup.id) {
+      const filteredBooks = books.filter(book => book.id !== popup.id);
+      setBooks(filteredBooks);
+      setPopup({
+        show: false,
+        id: null,
+      });
+      console.log('Delete item');
+    }
+  };
+
+  const handleDeleteFalse = () => {
+    setPopup({
+      show: false,
+      id: null,
+    });
+    console.log('Close popup');
+  };
 
   return (
     <GridTemplate pageType="books">
@@ -22,13 +63,21 @@ const Books = () => {
         books.map(book => (
           <Card
             cardType="books"
+            book={book}
             id={book.id}
             title={book.title}
-            author={book.author}
+            firstName={book.firstName}
+            lastName={book.lastName}
             imageUrl={book.imageUrl}
+            date={book.date}
+            content={books.content}
             key={book.id}
+            handleDelete={handleDelete}
           />
         ))}
+      {popup.show && (
+        <Popup handleDeleteTrue={handleDeleteTrue} handleDeleteFalse={handleDeleteFalse} />
+      )}
     </GridTemplate>
   );
 };
