e5fe0d53ec23a5354317cba492e1bc0c2db2eea0
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-11-28 21:12:20 +0100

    add univ. logo to background image for all pages

--- src/components/molecules/Card/index.js
+++ src/components/molecules/Card/index.js
@@ -1,7 +1,7 @@
 import {useState} from 'react';
 import PropTypes from 'prop-types';
 import { Arrow } from 'components/atoms/Arrow';
-import LogoSmall from 'assets/icons/mblSmall.svg';
+import { Link } from 'react-router-dom';
 
 import {
   StyledImage,
@@ -12,50 +12,20 @@ import {
   StyledDate,
   ButtonContentWrapper,
   ShowMore,
-  // StyledLink,
 } from './styled';
 import Wrapper from 'components/atoms/Wrapper';
 import trashIcon from 'assets/icons/trash.svg';
 
-const Card = ({ book, author, note, cardType,handleDelete }) => {
+const Card = ({ book, author, note, cardType, handleDelete }) => {
       const [showMore, setShowMore] = useState(false);
  
-      // if (cardType === 'notes') {
-      //   return (
-      // <Wrapper notes>
-      //   <InnerWrapper>
-      //     <>
-      //       <StyledImage src={note.fields.imageUrl} icon={LogoSmall}/>
-      //       <StyledTitle>{note.fields.title} </StyledTitle>
-      //       <StyledDate>{note.fields.date}</StyledDate>
-      //       {note.fields.content.length > 300 ? (
-      //         <>
-      //           {showMore ? note.fields.content : `${note.fields.content.slice(0, 300)}...`}
-      //           <ShowMore onClick={() => setShowMore(!showMore)}>
-      //             <ButtonContentWrapper>
-      //               <span>
-      //                 {showMore ? 'Show less' : 'Show more'}
-      //                 <Arrow showMore={showMore} />
-      //               </span>
-      //             </ButtonContentWrapper>
-      //           </ShowMore>
-      //         </>
-      //       ) : (
-      //         <>{note.fields.content}</>
-      //       )}
-      //     </>
-      //   </InnerWrapper>
-      // </Wrapper>
-      //   )}};
-  
-
-   
   return (
     <>
       {cardType === 'books' && (
         <Wrapper>
-          <InnerWrapper>
-            <StyledImage src={book.fields.imageUrl} />
+          <InnerWrapper >
+            <Link to={`books/${book.id}`}>
+              <StyledImage src={book.fields.imageUrl} /></Link>
             <StyledTitle>{book.fields.title}</StyledTitle>
             <StyledAuthor>
               {book.fields.firstName} {book.fields.lastName}
@@ -80,7 +50,7 @@ const Card = ({ book, author, note, cardType,handleDelete }) => {
         <Wrapper notes>
           <InnerWrapper>
             <>
-              <StyledImage src={note.fields.imageUrl} icon={LogoSmall} />
+              <StyledImage src={note.fields.imageUrl} />
               <StyledTitle>{note.fields.title} </StyledTitle>
               <StyledDate>{note.fields.date}</StyledDate>
               {note.fields.content.length > 300 ? (
--- src/components/molecules/Card/styled.js
+++ src/components/molecules/Card/styled.js
@@ -1,18 +1,19 @@
 import styled from 'styled-components';
 import Paragraph from 'components/atoms/Paragraph/Paragraph';
 import Wrapper from 'components/atoms/Wrapper';
-import { Link } from 'react-router-dom';
 import ButtonIcon from 'components/atoms/ButtonIcon/ButtonIcon';
+import LogoSmall from 'assets/icons/mblSmall.svg';
 
 export const StyledImage = styled.img`
   width: 165px;
   height: 225px;
   background-color: ${({ theme }) => theme.colors.silver};
-  background-image: url(${({ icon }) => icon});
+  background-image: url(${LogoSmall});
   background-position: 50% 50%;
   background-repeat: no-repeat;
-  background-size: 50%;
+  background-size: 45%;
   margin-bottom: 10px;
+  cursor: pointer;
 
   ${Wrapper}:hover & {
     background-color: ${({ theme }) => theme.colors.lipstick};
@@ -25,6 +26,7 @@ export const StyledTitle = styled.h1`
   font-weight: ${({ theme }) => theme.font.bold};
   text-align: center;
   margin-top: 10px 0 0;
+  text-decoration: none;
 
   ${Wrapper}:hover & {
     color: white;
@@ -37,6 +39,7 @@ export const StyledAuthor = styled.h2`
   font-weight: ${({ theme }) => theme.font.regular};
   margin: 0;
   text-align: center;
+  text-decoration: none;
 
   ${Wrapper}:hover & {
     color: white;
@@ -55,10 +58,8 @@ export const InnerWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-`;
-
-export const StyledLink = styled(Link)`
-  text-decoration: none;
+  /* text-decoration: none;
+  color: ${({theme}) => theme.colors.black} */
 `;
 
 export const TrashButton = styled(ButtonIcon)`
@@ -66,6 +67,7 @@ export const TrashButton = styled(ButtonIcon)`
   width: 35px;
   height: 35px;
   border-radius: 50%;
+  z-index: 5;
 `;
 
 
