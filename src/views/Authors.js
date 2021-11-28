4e2cb14509c5b25edc89ffd776b4454bea05dec6
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-11-28 14:23:16 +0100

    add useEffect and missing PropTypes

--- src/views/Authors.js
+++ src/views/Authors.js
@@ -1,30 +1,42 @@
 import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import GridTemplate from 'templates/GridTemplate';
+import { base } from 'airtable/base';
 import Card from 'components/molecules/Card';
-import { fetchAuthors } from 'api/authors';
 
 const Authors = () => {
   const [authors, setAuthors] = useState([]);
 
-  const fetchData = async () => {
-    const data = await fetchAuthors();
-    setAuthors(data);
-  };
-  useEffect(() => {
-    fetchData();
-  }, []);
+   useEffect(() => {
+     base('authors')
+       .select({ view: 'Grid view' })
+       .eachPage(
+         (records, fetchNextPage) => {
+           setAuthors(records);
+           fetchNextPage();
+           console.log(records);
+         },
+         function (err) {
+           if (err) {
+             console.error(err);
+             return;
+           }
+         },
+       );
+   }, []);
 
   return (
     <GridTemplate pageType="authors">
-      {authors.map(author => (
+      {authors && 
+      authors.map(author => (
         <Card
           cardType="authors"
+          author={author}
           id={author.id}
+          key={author.id}
+          imageUrl={author.imageUrl}
           firstName={author.firstName}
           lastName={author.lastName}
-          imageUrl={author.imageUrl}
-          key={author.id}
         />
       ))}
     </GridTemplate>
@@ -36,7 +48,8 @@ Authors.propTypes = {
     PropTypes.shape({
       id: PropTypes.string.isRequired,
       imageUrl: PropTypes.string.isRequired,
-      author: PropTypes.string.isRequired,
+      firstName: PropTypes.string.isRequired,
+      lastName: PropTypes.string.isRequired,
     }),
   ),
 };
