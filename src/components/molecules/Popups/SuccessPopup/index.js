35710bc7b70337b50cb147ab18dc85e39e5ea219
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-11-28 01:02:38 +0100

    add closing the popup after click the button

--- src/components/molecules/Popups/SuccessPopup/index.js
+++ src/components/molecules/Popups/SuccessPopup/index.js
@@ -1,33 +1,27 @@
-import React, {useState} from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import Button from 'components/atoms/Button/Button';
 import closeIcon from 'assets/icons/close.svg';
 import { StyledWrapper, ButtonWrapper, StyledParagraph, CloseButton } from './styled';
 import { Link } from 'react-router-dom';
 
-const SuccessPopup = () => {
-
-  const  [popup, setPopup] = useState({
-    show: true,
-    id: null
-  });
-const closePopup = () => {
-  {popup && 
-  setPopup({
-    show: false,
-    id: null,
-  });
-  console.log('Close popup');
-}};
+const SuccessPopup = ({ handlePopupClose }) => {
 
   return (
     <StyledWrapper>
       <StyledParagraph>Successfully submitted.</StyledParagraph>
-      <CloseButton icon={closeIcon} type='button' onClick={closePopup}/>
+      <CloseButton icon={closeIcon} type="button" onClick={handlePopupClose} />
       <ButtonWrapper>
-        <Button as={Link} to='/books'>Back to books</Button>
+        <Button as={Link} to="/books" onClick={handlePopupClose}>
+          Back to books
+        </Button>
       </ButtonWrapper>
     </StyledWrapper>
   );
+};
+
+SuccessPopup.propTypes = {
+  handlePopupClose: PropTypes.bool.isRequired,
 };
 
 export default SuccessPopup;
