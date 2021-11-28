2c0f3cb6bda1abbc8bc28fb92d2daaa9cdc99a82
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-11-28 16:35:45 +0100

    adjust the card to the note view

--- src/components/molecules/Card/index.js
+++ src/components/molecules/Card/index.js
@@ -1,4 +1,8 @@
+import {useState} from 'react';
 import PropTypes from 'prop-types';
+import { Arrow } from 'components/atoms/Arrow';
+import LogoSmall from 'assets/icons/mblSmall.svg';
+
 import {
   StyledImage,
   StyledTitle,
@@ -6,48 +10,101 @@ import {
   InnerWrapper,
   TrashButton,
   StyledDate,
+  ButtonContentWrapper,
+  ShowMore,
   // StyledLink,
 } from './styled';
 import Wrapper from 'components/atoms/Wrapper';
-import Paragraph from 'components/atoms/Paragraph/Paragraph';
 import trashIcon from 'assets/icons/trash.svg';
 
-const Card = ({ book, author, note, cardType, handleDelete }) => {
+const Card = ({ book, author, note, cardType,handleDelete }) => {
+      const [showMore, setShowMore] = useState(false);
+ 
+      // if (cardType === 'notes') {
+      //   return (
+      // <Wrapper notes>
+      //   <InnerWrapper>
+      //     <>
+      //       <StyledImage src={note.fields.imageUrl} icon={LogoSmall}/>
+      //       <StyledTitle>{note.fields.title} </StyledTitle>
+      //       <StyledDate>{note.fields.date}</StyledDate>
+      //       {note.fields.content.length > 300 ? (
+      //         <>
+      //           {showMore ? note.fields.content : `${note.fields.content.slice(0, 300)}...`}
+      //           <ShowMore onClick={() => setShowMore(!showMore)}>
+      //             <ButtonContentWrapper>
+      //               <span>
+      //                 {showMore ? 'Show less' : 'Show more'}
+      //                 <Arrow showMore={showMore} />
+      //               </span>
+      //             </ButtonContentWrapper>
+      //           </ShowMore>
+      //         </>
+      //       ) : (
+      //         <>{note.fields.content}</>
+      //       )}
+      //     </>
+      //   </InnerWrapper>
+      // </Wrapper>
+      //   )}};
+  
+
+   
   return (
-    <Wrapper>
-      <InnerWrapper>
-        {cardType === 'books' && (
-          <>
+    <>
+      {cardType === 'books' && (
+        <Wrapper>
+          <InnerWrapper>
             <StyledImage src={book.fields.imageUrl} />
             <StyledTitle>{book.fields.title}</StyledTitle>
             <StyledAuthor>
               {book.fields.firstName} {book.fields.lastName}
             </StyledAuthor>
-          </>
-        )}
+          </InnerWrapper>
+          <TrashButton icon={trashIcon} onClick={handleDelete} />
+        </Wrapper>
+      )}
 
-        {cardType === 'authors' && (
-          <>
+      {cardType === 'authors' && (
+        <Wrapper>
+          <InnerWrapper>
             <StyledImage src={author.fields.imageUrl} />
             <StyledTitle>
               {author.fields.firstName} {author.fields.lastName}
             </StyledTitle>
-          </>
-        )}
+          </InnerWrapper>
+        </Wrapper>
+      )}
 
-        {cardType === 'notes' && (
-          <>
-            {note.fields.imageUrl && <StyledImage src={note.fields.imageUrl} />}
-            <StyledTitle>{note.fields.title} </StyledTitle>
-            <StyledDate>{note.fields.date}</StyledDate>
-            <Paragraph>{note.fields.content}</Paragraph>
-          </>
-        )}
-        <TrashButton icon={trashIcon} onClick={handleDelete} />
-      </InnerWrapper>
-    </Wrapper>
-  );
-};
+      {cardType === 'notes' && (
+        <Wrapper notes>
+          <InnerWrapper>
+            <>
+              <StyledImage src={note.fields.imageUrl} icon={LogoSmall} />
+              <StyledTitle>{note.fields.title} </StyledTitle>
+              <StyledDate>{note.fields.date}</StyledDate>
+              {note.fields.content.length > 300 ? (
+                <>
+                  {showMore ? note.fields.content : `${note.fields.content.slice(0, 300)}...`}
+                  <ShowMore onClick={() => setShowMore(!showMore)}>
+                    <ButtonContentWrapper>
+                      <span>
+                        {showMore ? 'Show less' : 'Show more'}
+                        <Arrow showMore={showMore} />
+                      </span>
+                    </ButtonContentWrapper>
+                  </ShowMore>
+                </>
+              ) : (
+                <>{note.fields.content}</>
+              )}
+            </>
+          </InnerWrapper>
+        </Wrapper>
+      )}
+    </>
+  );};
+ 
 
 Card.propTypes = {
   cardType: PropTypes.oneOf(['home', 'books', 'authors', 'notes']),
--- src/components/molecules/Card/styled.js
+++ src/components/molecules/Card/styled.js
@@ -5,9 +5,19 @@ import { Link } from 'react-router-dom';
 import ButtonIcon from 'components/atoms/ButtonIcon/ButtonIcon';
 
 export const StyledImage = styled.img`
+  width: 165px;
   height: 225px;
-  background-color: transparent;
+  background-color: ${({ theme }) => theme.colors.silver};
+  background-image: url(${({ icon }) => icon});
+  background-position: 50% 50%;
+  background-repeat: no-repeat;
+  background-size: 50%;
   margin-bottom: 10px;
+  z-index: 999;
+
+  ${Wrapper}:hover & {
+    background-color: ${({ theme }) => theme.colors.lipstick};
+  }
 `;
 
 export const StyledTitle = styled.h1`
@@ -37,6 +47,9 @@ export const StyledAuthor = styled.h2`
 export const StyledParagraph = styled(Paragraph)`
   font-size: 1rem;
   color: ${({ theme }) => theme.colors.grey};
+  ${Wrapper}:hover & {
+    color: white;
+  }
 `;
 
 export const InnerWrapper = styled.div`
@@ -56,3 +69,29 @@ export const TrashButton = styled(ButtonIcon)`
   border-radius: 50%;
 `;
 
+
+export const StyledDate = styled.p``;
+
+export const ShowMore = styled.button`
+  display: block;
+  border: none;
+  background: none;
+  padding: 2px;
+  text-transform: uppercase;
+  font-size: 12px;
+  font-weight: 500;
+  margin: 0 auto 0;
+  color: ${({ theme }) => theme.colors.lipstick};
+  padding: 10px;
+
+  ${Wrapper}:hover & {
+    color: white;
+  }
+`;
+
+export const ButtonContentWrapper = styled.span`
+  display: grid;
+  grid-template-columns: auto auto;
+  grid-gap: 6px;
+  align-items: center;
+`;
