682fa2109317da1b0d928b48e88fd567fb774a39
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-12-02 18:03:10 +0100

    add pageContext, change typ and action in Button

--- src/components/molecules/Popups/SuccessPopup/index.js
+++ src/components/molecules/Popups/SuccessPopup/index.js
@@ -3,16 +3,17 @@ import PropTypes from 'prop-types';
 import Button from 'components/atoms/Button/Button';
 import closeIcon from 'assets/icons/close.svg';
 import { StyledWrapper, ButtonWrapper, StyledParagraph, CloseButton } from './styled';
-import { Link } from 'react-router-dom';
 
-const SuccessPopup = ({ handlePopupClose }) => {
+import withContext from 'hoc/withContext';
+
+const SuccessPopup = ({ handlePopupClose, pageContext, toggleNewItemBar }) => {
   return (
     <StyledWrapper>
       <StyledParagraph>Successfully submitted.</StyledParagraph>
       <CloseButton icon={closeIcon} type="button" onClick={handlePopupClose} />
       <ButtonWrapper>
-        <Button as={Link} to="/books" onClick={handlePopupClose}>
-          Back to books
+        <Button type="button" onClick={toggleNewItemBar}>
+          Back to {pageContext}
         </Button>
       </ButtonWrapper>
     </StyledWrapper>
@@ -21,6 +22,8 @@ const SuccessPopup = ({ handlePopupClose }) => {
 
 SuccessPopup.propTypes = {
   handlePopupClose: PropTypes.func.isRequired,
+  pageContext: PropTypes.oneOf(['home', 'books', 'authors', 'notes']),
+  toggleNewItemBar: PropTypes.func,
 };
 
-export default SuccessPopup;
+export default withContext(SuccessPopup);
