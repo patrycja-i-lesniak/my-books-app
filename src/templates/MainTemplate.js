e36a4f10b37840d4021065c967e39cf2723dca62
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-12-03 19:40:47 +0100

    delete breakpoint from add new itam buttom

--- src/templates/MainTemplate.js
+++ src/templates/MainTemplate.js
@@ -1,3 +1,4 @@
+// import { useState, useEffect } from 'react';
 import styled, { ThemeProvider } from 'styled-components';
 import { Component } from 'react';
 import { withRouter } from 'react-router';
@@ -5,7 +6,6 @@ import PropTypes from 'prop-types';
 import { theme } from 'theme/theme';
 import { GlobalStyle } from 'theme/GlobalStyle';
 import PageContext from 'context';
-// import Modal from 'components/organisms/Modal';
 import { booksData, authorsData, notesData } from 'assets/booksData';
 import ButtonIcon from 'components/atoms/ButtonIcon/ButtonIcon';
 import plusIcon from 'assets/icons/plus.svg';
@@ -19,10 +19,6 @@ const StyledButtonIcon = styled(ButtonIcon)`
   right: 20px;
   z-index: 999;
   box-shadow: ${({ theme }) => theme.shadows.boxShadowDark};
-
-  @media (max-width: ${({ theme }) => theme.breakpoints.small}px) {
-    bottom: 80px;
-  }
 `;
 
 const initialBookState = booksData;
@@ -53,7 +49,7 @@ class MainTemplate extends Component {
       location: { pathname },
     } = this.props;
 
-    const [currentPage] = pageTypes.filter((page) => pathname.includes(page));
+    const [currentPage] = pageTypes.filter(page => pathname.includes(page));
 
     if (prevState.pageType !== currentPage) {
       this.setState({ pageType: currentPage });
@@ -82,7 +78,7 @@ class MainTemplate extends Component {
   // };
 
   toggleNewItemBar = () => {
-    this.setState((prevState) => ({
+    this.setState(prevState => ({
       isNewItemBarVisible: !prevState.isNewItemBarVisible,
     }));
   };
@@ -103,13 +99,13 @@ class MainTemplate extends Component {
             {pageType === 'home' ? null : (
               <StyledButtonIcon icon={plusIcon} onClick={this.toggleNewItemBar} />
             )}
-            <NewItemBar handleClose={this.toggleNewItemBar} isVisible={isNewItemBarVisible} />
+            <NewItemBar isVisible={isNewItemBarVisible} toggleNewItemBar={this.toggleNewItemBar} />
             {/* {isModalOpen && (
             <>
               <StyledBackdrop onClick={this.closeModal} />
               <Modal closeModalFn={this.closeModal} addItemFn={addItemFn} />
-            </> */}
-            {/* )} */}
+            </>
+            )} */}
           </ThemeProvider>
         </PageContext.Provider>
       </div>
