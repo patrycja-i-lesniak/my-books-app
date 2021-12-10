7b0346f7a82f5bcb37a2ce46080165f8b2cb77ca
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-12-10 08:15:28 +0100

    add condition rendering to endpoints

--- src/components/molecules/NewItemForm/index.js
+++ src/components/molecules/NewItemForm/index.js
@@ -1,58 +1,29 @@
 import React, { useState } from 'react';
-import { Formik, ErrorMessage, Form } from 'formik';
+import { Formik, ErrorMessage } from 'formik';
 import PropTypes from 'prop-types';
 import { axiosInstance, apiEndpoints } from 'airtable/base';
-import validationSchema from 'helpers/validationSchema';
-import Input from 'components/atoms/Input';
+import {
+  bookValidationSchema,
+  authorValidationSchema,
+  noteValidationSchema,
+} from 'components/molecules/NewItemForm/validationSchema';
 import RequiredBox from 'components/molecules/RequredBox/RequiredBox';
 import ErrorPopup from 'components/molecules/Popups/ErrorPopup';
 import SuccessPopup from 'components/molecules/Popups/SuccessPopup';
 import {
   StyledForm,
-  StyledTextArea,
-  RequiredWrapper,
-  NameContainer,
-  ContentContainer,
   StyledButton,
-  StyledSelect,
+  InputField,
+  StyledLabel,
+  InputWrapper,
+  DataWrapper,
 } from './styled';
 import withContext from 'hoc/withContext';
+import { initialValues } from './initialValues';
+import { Options } from './Options';
+import api from 'api';
 
-const Options = () => (
-  <>
-    <option value="" label="Select status" />
-    <option value="read" label="read" />
-    <option value="to read" label="to read" />
-    <option value="in progress" label="in progress" />
-    <option value="to buy" label="to buy" />
-    <option value="borrowed" label="borrowed" />
-  </>
-);
-
- const initialValues = {
-   type: '',
-   title: '',
-   firstName: '',
-   lastName: '',
-   imageUrl: 'https://',
-   series: '',
-   date: '',
-   dateOfBirth: '',
-   dateOfDeath: '',
-   isbn: '',
-   translation: '',
-   publishing: '',
-   numberOfPages: '',
-   content:
-     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-   LClink: 'https://',
-   status: '',
-   oficialWebsite: ',',
- };
-
- 
-
-const NewItemForm = ({pageContext, toggleNewItemBar}) => {
+const NewItemForm = ({ pageContext, toggleNewItemBar }) => {
   const [popup, setPopup] = useState(false);
   const [errorPopup, setErrorPopup] = useState(false);
 
@@ -62,11 +33,22 @@ const NewItemForm = ({pageContext, toggleNewItemBar}) => {
     console.log('Close popup');
   };
 
+  console.log(pageContext);
+
+  const endpoint = `/${pageContext}`;
+
   return (
     <Formik
       initialValues={initialValues}
-      validationSchema={validationSchema}
-      onSubmit={async values => {
+      validationSchema={
+        pageContext === 'books'
+          ? bookValidationSchema
+          : pageContext === 'authors'
+          ? authorValidationSchema
+          : noteValidationSchema
+      }
+      onSubmit={async (values, { resetForm }) => {
+        console.log(values);
         const book = {
           records: [
             {
@@ -74,49 +56,62 @@ const NewItemForm = ({pageContext, toggleNewItemBar}) => {
                 title: values.title,
                 firstName: values.firstName,
                 lastName: values.lastName,
-                // imageUrl: values.imageUrl,
-                // series: values.series,
-                // date: values.date,
-                // content: values.content,
+                imageUrl: values.imageUrl,
+                series: values.series,
+                date: values.date,
+                numberOfPages: values.numberOfPages,
+                isbn: values.isbn,
+                translation: values.translation,
+                publishing: values.publishing,
                 status: values.status,
-                // isbn: values.isbn,
+                content: values.content,
+                LClink: values.LClink,
               },
             },
           ],
         };
-        // const author = {
-        //   records: [
-        //     {
-        //       fields: {
-        //         firstName: values.firstName,
-        //         lastName: values.lastName,
-        //         content: values.content,
-        //       },
-        //     },
-        //   ],
-        // };
-        // const note = {
-        //   records: [
-        //     {
-        //       fields: {
-        //         title: values.title,
-        //         content: values.content,
-        //       },
-        //     },
-        //   ],
-        // };
 
-        await axiosInstance
+        const author = {
+          records: [
+            {
+              fields: {
+                firstName: values.firstName,
+                lastName: values.lastName,
+                content: values.content,
+                imageUrl: values.imageUrl,
+                dateOfBirth: values.dateOfBirth,
+                dateOfDeath: values.dateOfDeath,
+                oficialWebsite: values.oficialWebsite,
+              },
+            },
+          ],
+        };
+
+        const note = {
+          records: [
+            {
+              fields: {
+                title: values.title,
+                imageUrl: values.imageUrl,
+                date: values.date,
+                content: values.content,
+              },
+            },
+          ],
+        };
+
+        api
           .post(
-            apiEndpoints.booksList,
-            book,
-            // apiEndpoints.authorsList,
-            // author,
-            // apiEndpoints.notesList,
-            // note,
+            endpoint,
+            pageContext === 'books'
+              ? (endpoint, book)
+              : pageContext === 'authors'
+              ? (endpoint, author)
+              : (endpoint, note),
           )
           .then(response => {
             setPopup(true);
+            resetForm();
             console.log(response);
             console.log('Successfully submitted.');
           })
@@ -127,22 +122,31 @@ const NewItemForm = ({pageContext, toggleNewItemBar}) => {
           });
       }}
     >
-      {({ values, handleChange, handleBlur, handleSubmit, errors }) => (
+      {({ values, handleChange, handleBlur, handleSubmit, handleReset }) => (
         <StyledForm method="POST" autoComplete="off" onSubmit={handleSubmit}>
           <>
             {errorPopup && (
-              <ErrorPopup handlePopupClose={handlePopupClose} toggleNewItemBar={toggleNewItemBar} />
+              <ErrorPopup
+                handlePopupClose={handlePopupClose}
+                toggleNewItemBar={toggleNewItemBar}
+                handleReset={handleReset}
+              />
             )}
             {popup && (
               <SuccessPopup
                 handlePopupClose={handlePopupClose}
                 toggleNewItemBar={toggleNewItemBar}
+                handleReset={handleReset}
               />
             )}
           </>
           {pageContext === 'authors' ? null : (
             <>
-              <Input
+              <StyledLabel as="label" htmlFor="title">
+                title
+              </StyledLabel>
+              <InputField
+                as="input"
                 type="text"
                 name="title"
                 onChange={handleChange}
@@ -150,166 +154,214 @@ const NewItemForm = ({pageContext, toggleNewItemBar}) => {
                 value={values.title}
                 autoComplete="given-name"
               />
+
               <ErrorMessage name="title">{msg => <RequiredBox msg={msg} />}</ErrorMessage>
             </>
           )}
-          <RequiredWrapper>
-            <NameContainer>
-              {pageContext === 'notes' ? null : (
-                <>
-                  <Input
-                    type="text"
-                    name="firstName"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.firstName}
-                  />
-                  <ErrorMessage name="firstName">{msg => <RequiredBox msg={msg} />}</ErrorMessage>
-                </>
-              )}
-            </NameContainer>
-            <NameContainer>
-              {pageContext === 'notes' ? null : (
-                <>
-                  <Input
-                    type="text"
-                    name="lastName"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.lastName}
-                  />
-                  <ErrorMessage name="lastName">{msg => <RequiredBox msg={msg} />}</ErrorMessage>
-                </>
-              )}
-            </NameContainer>
-          </RequiredWrapper>
-          <Input
-            type="url"
-            name="imageUrl"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.imageUrl}
-          />
-          {pageContext === 'books' && (
-            <Input
-              type="text"
-              name="series"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.series}
-            />
+
+          {pageContext === 'notes' ? null : (
+            <InputWrapper>
+              <DataWrapper>
+                <StyledLabel htmlFor="first name">first name</StyledLabel>
+                <InputField
+                  as="input"
+                  type="text"
+                  name="firstName"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.firstName}
+                />
+                <ErrorMessage name="firstName">{msg => <RequiredBox msg={msg} />}</ErrorMessage>
+              </DataWrapper>
+              <DataWrapper>
+                <StyledLabel htmlFor="last name">last name</StyledLabel>
+                <InputField
+                  as="input"
+                  type="text"
+                  name="lastName"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.lastName}
+                />
+                <ErrorMessage name="lastName">{msg => <RequiredBox msg={msg} />}</ErrorMessage>
+              </DataWrapper>
+            </InputWrapper>
           )}
-          {/* {pageContext === 'authors' ? (
-              <> 
-            <Input
-              type="date"
-              name="dateOfBirth"
+          <>
+            <StyledLabel htmlFor="image url">image url</StyledLabel>
+            <InputField
+              as="input"
+              type="url"
+              name="imageUrl"
               onChange={handleChange}
               onBlur={handleBlur}
-              value={values.dateOfBirth}
+              value={values.imageUrl}
             />
-            {/*
-                <Input
+            <ErrorMessage name="imageUrl">{msg => <RequiredBox msg={msg} />}</ErrorMessage>
+          </>
+          {pageContext === 'books' && (
+            <>
+              <StyledLabel htmlFor="series">series</StyledLabel>
+              <InputField
+                as="input"
+                type="text"
+                name="series"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.series}
+              />
+              <ErrorMessage name="series">{msg => <RequiredBox msg={msg} />}</ErrorMessage>
+            </>
+          )}
+
+          {pageContext === 'authors' ? (
+            <InputWrapper>
+              <DataWrapper>
+                <StyledLabel htmlFor="date of birth">date of birth</StyledLabel>
+                <InputField
+                  as="input"
+                  type="date"
+                  name="dateOfBirth"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.dateOfBirth}
+                />
+                <ErrorMessage name="dateOfBirth">{msg => <RequiredBox msg={msg} />}</ErrorMessage>
+              </DataWrapper>
+              <DataWrapper>
+                <StyledLabel htmlFor="date of death">date of death</StyledLabel>
+                <InputField
+                  as="input"
                   type="date"
                   name="dateOfDeath"
                   onChange={handleChange}
                   onBlur={handleBlur}
                   value={values.dateOfDeath}
                 />
-              </>
-            ) : (*/}
-          <Input
-            type="date"
-            name="date"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.date}
-          />
-          {/* )} */}
-
-          {pageContext === 'books' && (
-            <Input
-              type="number"
-              name="isbn"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.isbn}
-            />
-          )}
-          {pageContext === 'books' && (
-            <Input
-              type="text"
-              name="translation"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.translation}
-            />
-          )}
-          {pageContext === 'books' && (
-            <Input
-              type="text"
-              name="publishing"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.publishing}
-            />
+                <ErrorMessage name="dateOfDeath">{msg => <RequiredBox msg={msg} />}</ErrorMessage>
+              </DataWrapper>
+            </InputWrapper>
+          ) : (
+            <>
+              <StyledLabel htmlFor="date">date of publication</StyledLabel>
+              <InputField
+                as="input"
+                type="date"
+                name="date"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.date}
+              />
+              <ErrorMessage name="date">{msg => <RequiredBox msg={msg} />}</ErrorMessage>
+            </>
           )}
 
           {pageContext === 'books' && (
-            <Input
-              type="number"
-              name="numberOfPages"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.numberOfPages}
-              maxLenght="4"
-            />
-          )}
+            <>
+              <InputWrapper>
+                <DataWrapper>
+                  <StyledLabel htmlFor="ISBN">ISBN</StyledLabel>
+                  <InputField
+                    as="input"
+                    type="number"
+                    name="isbn"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.isbn}
+                  />
+                  <ErrorMessage name="isbn">{msg => <RequiredBox msg={msg} />}</ErrorMessage>
+                </DataWrapper>
+                <DataWrapper>
+                  <StyledLabel htmlFor="number of pages">number of pages</StyledLabel>
+                  <InputField
+                    as="input"
+                    type="number"
+                    name="numberOfPages"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.numberOfPages}
+                    maxLenght="4"
+                  />
+                  <ErrorMessage name="numberOfPages">
+                    {msg => <RequiredBox msg={msg} />}
+                  </ErrorMessage>
+                </DataWrapper>
+              </InputWrapper>
 
-          {pageContext === 'books' && (
-            <Input
-              type="url"
-              name="LClink"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.LClink}
-            />
+              <StyledLabel htmlFor="translation">translation</StyledLabel>
+              <InputField
+                as="input"
+                type="text"
+                name="translation"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.translation}
+              />
+              <ErrorMessage name="translation">{msg => <RequiredBox msg={msg} />}</ErrorMessage>
+
+              <StyledLabel htmlFor="publishing">publishing</StyledLabel>
+              <InputField
+                as="input"
+                type="text"
+                name="publishing"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.publishing}
+              />
+              <ErrorMessage name="publishing">{msg => <RequiredBox msg={msg} />}</ErrorMessage>
+
+              <StyledLabel htmlFor="link LubimyCytać.pl">link LubimyCytać.pl</StyledLabel>
+              <InputField
+                as="input"
+                type="url"
+                name="LClink"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.LClink}
+              />
+              <ErrorMessage name="LClink">{msg => <RequiredBox msg={msg} />}</ErrorMessage>
+
+              <InputField
+                select
+                as="select"
+                name="status"
+                value={values.status}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                style={{ display: 'block' }}
+              >
+                <Options />
+              </InputField>
+              <ErrorMessage name="status">{msg => <RequiredBox msg={msg} />}</ErrorMessage>
+            </>
           )}
-          {/* {pageContext === 'authors' && (
-              <Input
+          {pageContext === 'authors' && (
+            <>
+              <StyledLabel htmlFor="oficial website">oficial website</StyledLabel>
+              <InputField
+                as="input"
                 type="url"
                 name="oficialWebsite"
                 onChange={handleChange}
                 onBlur={handleBlur}
                 value={values.oficialWebsite}
               />
-            )} */}
-          {pageContext === 'books' && (
-            <StyledSelect
-              as="select"
-              name="status"
-              value={values.status}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              style={{ display: 'block' }}
-            >
-              <Options />
-            </StyledSelect>
+              <ErrorMessage name="oficialWebsite">{msg => <RequiredBox msg={msg} />}</ErrorMessage>
+            </>
           )}
 
-          <ContentContainer>
-            <StyledTextArea
-              type="text"
-              name="content"
-              as="textarea"
-              placeholder="add description"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.content}
-            />
-            <ErrorMessage name="content">{msg => <RequiredBox msg={msg} />}</ErrorMessage>
-          </ContentContainer>
-
+          <StyledLabel htmlFor="content">description</StyledLabel>
+          <InputField
+            textarea
+            type="text"
+            name="content"
+            as="textarea"
+            placeholder="add description"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.content}
+          />
+          <ErrorMessage name="content">{msg => <RequiredBox msg={msg} />}</ErrorMessage>
           <StyledButton type="submit">Add new item</StyledButton>
         </StyledForm>
       )}
