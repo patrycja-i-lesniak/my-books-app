04dd26f95c3f74be780063bab305855a02316b0e
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-12-12 19:31:13 +0100

    move Opinions component to separate files

---
+++ src/components/molecules/NewItemForm/Options.js
@@ -0,0 +1,10 @@
+export const Options = () => (
+  <>
+    <option value="" label="Select status" />
+    <option value="read" label="read" />
+    <option value="to read" label="to read" />
+    <option value="in progress" label="in progress" />
+    <option value="to buy" label="to buy" />
+    <option value="borrowed" label="borrowed" />
+  </>
+);
