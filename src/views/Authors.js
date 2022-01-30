749fcb303029f70b66b649f42562d65aa7a33df6
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-01-30 11:11:39 +0100

    change pageSize, organize the code

--- src/views/Authors.js
+++ src/views/Authors.js
@@ -7,35 +7,30 @@ import CardSmall from 'components/molecules/Card/CardSmall';
 const Authors = () => {
   const [authors, setAuthors] = useState([]);
 
-   useEffect(() => {
-     base('authors')
-       .select({ view: 'Grid view', pageSize: 12, sort: [{ field: 'lastName' }] })
-       .eachPage(
-         (records, fetchNextPage) => {
-           setAuthors(records);
-           fetchNextPage();
-           console.log(records);
-         },
-         function (err) {
-           if (err) {
-             console.error(err);
-             return;
-           }
-         },
-       );
-   }, []);
+  useEffect(() => {
+    base('authors')
+      .select({ view: 'Grid view', pageSize: 24, sort: [{ field: 'lastName'}] })
+      .eachPage(
+        (records, fetchNextPage) => {
+          setAuthors(records);
+          fetchNextPage();
+          console.log(records);
+        },
+        function (err) {
+          if (err) {
+            console.error(err);
+            return;
+          }
+        },
+      );
+  }, []);
 
   return (
     <GridTemplate pageType="authors">
-      {authors && 
-      authors.map(author => (
-        <CardSmall
-          cardType="authors"
-          author={author}
-          id={author.id}
-          key={author.id}
-        />
-      ))}
+      {authors &&
+        authors.map(author => (
+          <CardSmall cardType="authors" author={author} id={author.id} key={author.id} />
+        ))}
     </GridTemplate>
   );
 };
