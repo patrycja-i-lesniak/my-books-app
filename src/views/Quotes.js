e4e358a4e644b2e2c1d8b8ab408e754ca6d6eb60
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-02-03 02:09:13 +0100

    ad Masonry layout to Quotes view

--- src/views/Quotes.js
+++ src/views/Quotes.js
@@ -1,12 +1,40 @@
+import { useState, useEffect } from 'react';
+
 import GridTemplate from 'templates/GridTemplate';
 import NewQuoteForm from 'components/molecules/NewQuoteForm';
+import QuoteCard from 'components/molecules/Card/QuoteCard';
+import Masonry from 'components/molecules/Masonry';
+import { base } from 'airtable/base';
 
-const Quotes = () => (
-  <>
+const Quotes = () => {
+  const [quotes, setQuotes] = useState();
+
+  useEffect(() => {
+    base('quotes')
+      .select({ view: 'Grid view', pageSize: 100 })
+      .eachPage(
+        (records, fetchNextPage) => {
+          setQuotes(records);
+          fetchNextPage();
+          console.log('quotes:', records);
+        },
+        function (err) {
+          if (err) {
+            console.error(err);
+            return;
+          }
+        },
+      );
+  }, []);
+
+  return (
     <GridTemplate pageType="quotes">
       <NewQuoteForm />
+        <Masonry column3={3}>
+          {quotes && quotes.map(quote => <QuoteCard quote={quote} key={quote.id} />)}
+        </Masonry>
     </GridTemplate>
-  </>
-);
+  );
+};
 
 export default Quotes;
