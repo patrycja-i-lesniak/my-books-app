fbf3201fce05ce52b0ddf432efb7ddd4cfb33402
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-02-06 22:51:19 +0100

    add checked to validation, change validation for author fileds

--- src/components/molecules/NewQuoteForm/validationSchema.js
+++ src/components/molecules/NewQuoteForm/validationSchema.js
@@ -6,6 +6,7 @@ Yup.addMethod(Yup.date, 'stripEmptySDate', function () {
 
 export const validationSchema = Yup.object().shape({
   quote: Yup.string().required('Required field is empty'),
-  author: Yup.string().required('Required field is empty'),
+  author: Yup.string().min(2, 'Too Short!'),
   title: Yup.string().min(2, 'Too Short!').max(50, 'Too Long!'),
+  checked:  Yup.boolean().oneOf([true, false]),
 });
