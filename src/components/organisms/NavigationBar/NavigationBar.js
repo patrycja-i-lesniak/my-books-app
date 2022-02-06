2566bb2fda2aed338522ab2af4b1babb60d9c2c2
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-02-06 22:40:47 +0100

    move styles to separate file

--- src/components/organisms/NavigationBar/NavigationBar.js
+++ src/components/organisms/NavigationBar/NavigationBar.js
@@ -1,98 +1,14 @@
 import { NavLink } from 'react-router-dom';
-import styled from 'styled-components';
-import myBooksLogoBlack from 'assets/icons/myBooksLogoBlack.svg';
-import Button from 'components/atoms/Button/Button';
+import {
+  StyledWrapper,
+  StyledNavigationWrapper,
+  StyledLogoLink,
+  StyledLinkList,
+  StyledItem,
+  StyledButton,
+} from './styled';
+
 import SearchBox from 'components/atoms/SearchBox';
-
-const StyledWrapper = styled.div`
-  display: grid;
-  position: fixed;
-  top: 0;
-  width: 100vw;
-  background-color: ${({ theme }) => theme.colors.lipstick};
-  grid-template-columns: 1fr 280px 120px;
-  align-items: end;
-  height: 80px;
-  padding: 20px 40px;
-  z-index: 9999;
-  box-shadow: ${({ theme }) => theme.shadows.boxShadowDark};
-
-  @media (max-width: ${({ theme }) => theme.breakpoints.large}px) {
-    grid-template-columns: 1fr 120px;
-  }
-
-  @media (max-width: ${({ theme }) => theme.breakpoints.medium}px) {
-    grid-template-columns: 1fr;
-  }
-
-  @media (max-width: ${({ theme }) => theme.breakpoints.small}px) {
-    height: 60px;
-    padding: 0;
-    align-items: center;
-  }
-`;
-
-const StyledNavigationWrapper = styled.nav`
-  display: grid;
-  grid-template-columns: repeat(4, auto);
-  align-items: end;
-  justify-content: start;
-
-  @media (max-width: ${({ theme }) => theme.breakpoints.medium}px) {
-    justify-content: center;
-  }
-`;
-
-const StyledLogoLink = styled(NavLink)`
-  display: block;
-  width: 200px;
-  height: 40px;
-  background-position: 50% 50%;
-  background-image: url(${myBooksLogoBlack});
-  background-repeat: no-repeat;
-
-  @media (max-width: ${({ theme }) => theme.breakpoints.small}px) {
-    width: 150px;
-  }
-`;
-
-const StyledLinkList = styled.ul`
-  margin: 0;
-  list-style: none;
-
-  @media (max-width: ${({ theme }) => theme.breakpoints.medium}px) {
-    padding: 0;
-  }
-`;
-
-const activeClassName = 'active';
-
-const StyledItem = styled(NavLink).attrs(() => ({ activeClassName }))`
-  padding: 0 20px 0 0;
-  list-style: none;
-  text-decoration: none;
-  font-size: 1.7rem;
-  color: ${({ theme }) => theme.colors.white};
-
-  @media (max-width: ${({ theme }) => theme.breakpoints.medium}px) {
-    display: none;
-  }
-
-  &.${activeClassName} {
-    font-weight: 700;
-  }
-`;
-
-const StyledButton = styled(Button)`
-  font-weight: 600;
-  font-size: 1.6rem;
-  height: 36px;
-  text-transform: uppercase;
-
-  @media (max-width: ${({ theme }) => theme.breakpoints.medium}px) {
-    display: none;
-  }
-`;
 
 const NavigationBar = () => (
   <StyledWrapper>
---
+++ src/components/organisms/NavigationBar/styled.js
@@ -0,0 +1,96 @@
+import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
+
+import myBooksLogoBlack from 'assets/icons/myBooksLogoBlack.svg';
+import Button from 'components/atoms/Button/Button';
+
+export const StyledWrapper = styled.div`
+  display: grid;
+  position: fixed;
+  top: 0;
+  width: 100vw;
+  background-color: ${({ theme }) => theme.colors.lipstick};
+  grid-template-columns: 1fr 280px 120px;
+  align-items: end;
+  height: 80px;
+  padding: 20px 40px;
+  z-index: 9999;
+  box-shadow: ${({ theme }) => theme.shadows.boxShadowDark};
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.large}px) {
+    grid-template-columns: 1fr 120px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.medium}px) {
+    grid-template-columns: 1fr;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.small}px) {
+    height: 60px;
+    padding: 0;
+    align-items: center;
+  }
+`;
+
+export const StyledNavigationWrapper = styled.nav`
+  display: grid;
+  grid-template-columns: repeat(4, auto);
+  align-items: end;
+  justify-content: start;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.medium}px) {
+    justify-content: center;
+  }
+`;
+
+export const StyledLogoLink = styled(NavLink)`
+  display: block;
+  width: 200px;
+  height: 40px;
+  background-position: 50% 50%;
+  background-image: url(${myBooksLogoBlack});
+  background-repeat: no-repeat;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.small}px) {
+    width: 150px;
+  }
+`;
+
+export const StyledLinkList = styled.ul`
+  margin: 0;
+  list-style: none;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.medium}px) {
+    padding: 0;
+  }
+`;
+
+const activeClassName = 'active';
+
+export const StyledItem = styled(NavLink).attrs(() => ({ activeClassName }))`
+  padding: 0 20px 0 0;
+  list-style: none;
+  text-decoration: none;
+  font-size: 1.7rem;
+  color: ${({ theme }) => theme.colors.white};
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.medium}px) {
+    display: none;
+  }
+
+  &.${activeClassName} {
+    font-weight: 700;
+  }
+`;
+
+export const StyledButton = styled(Button)`
+  font-weight: 600;
+  font-size: 1.6rem;
+  height: 36px;
+  text-transform: uppercase;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.medium}px) {
+    display: none;
+  }
+`;
+
