a65baa4c81e0847573a77541a9cfb99394f0eeb1
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-02-19 21:29:34 +0100

    add validationSchema for QuoteCard

---
+++ src/components/molecules/Card/QuoteCard/validationSchema.js
@@ -0,0 +1,12 @@
+import * as Yup from 'yup';
+
+Yup.addMethod(Yup.date, 'stripEmptySDate', function () {
+  return this.transform(value => (value === '' ? undefined : value));
+});
+
+export const validationSchema = Yup.object().shape({
+  quote: Yup.string().required('Required field is empty'),
+  author: Yup.string(),
+  title: Yup.string().min(2, 'Too Short!').max(50, 'Too Long!'),
+  checked: Yup.boolean().oneOf([false, true]),
+});
