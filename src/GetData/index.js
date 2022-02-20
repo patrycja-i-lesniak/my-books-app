d037cc81bc3ecfe9f76f6ac4b8905d80ec81975d
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-02-20 23:41:22 +0100

    rename file

--- src/GetData/index.js
+++ src/GetData/index.js
@@ -1,1 +1,10 @@
-export { default as GetData } from './GetData';
+import CardSmall from 'components/molecules/Card/CardSmall';
+import { useFetchData } from 'customHooks';
+
+const GetData = data => {
+  const { items } = useFetchData(data);
+
+  return <>{items && items.map(item => <CardSmall item={item} key={item.id} />)}</>;
+};
+
+export default GetData;
