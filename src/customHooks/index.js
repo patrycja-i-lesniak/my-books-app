8b72288cb3bd62fd2893460381b04376918de8a3
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-02-22 21:54:26 +0100

    add index file with imports to all custom hooks

--- src/customHooks/index.js
+++ src/customHooks/index.js
@@ -1,3 +1,4 @@
 export { default as useFetchData } from './useFetchData';
+export { default as useFetchDetailsData } from './useFetchDetailsData';
 export { default as useGetCurrentPage } from './useGetCurrentPage';
 export { default as useLocalStorage } from './useLocalStorage';
