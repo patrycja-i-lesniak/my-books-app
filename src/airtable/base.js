b5b2e18ae48917ddbc71e12e8cedc5f682b360f1
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-02-25 18:54:08 +0100

    format code, delete unnecessary data

--- src/airtable/base.js
+++ src/airtable/base.js
@@ -2,5 +2,6 @@ import Airtable from 'airtable';
 
 const { REACT_APP_DB_ID, REACT_APP_API_KEY } = process.env;
 
+export const BASE_URL = `https://api.airtable.com/v0/${REACT_APP_DB_ID}`;
 
-export const base = new Airtable({ apiKey: `${REACT_APP_API_KEY}`}).base(`${REACT_APP_DB_ID}`);
+export const base = new Airtable({ apiKey: `${REACT_APP_API_KEY}` }).base(`${REACT_APP_DB_ID}`);
