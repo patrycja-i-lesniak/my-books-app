cdc6853c6d7d2431be334a1084a9e259081e7c16
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-02-27 00:17:13 +0100

    add Masonry component to GetData

--- src/GetData/index.js
+++ src/GetData/index.js
@@ -1,28 +1,40 @@
 import PropTypes from 'prop-types';
 
-import { CardSmall, QuoteCard } from 'components/molecules/Card/index';
+import { CardSmall, QuoteCard, QuoteCardSmall } from 'components/molecules/Card/index';
 import { useFetchData } from 'customHooks';
 import withContext from 'hoc/withContext';
+
+import Masonry from 'react-masonry-css';
+import './styles.css';
+
+const breakpointColumnsObj = {
+  default: 3,
+  900: 2,
+  600: 1,
+};
 
 const GetData = ({ data, pageContext }) => {
   const { items } = useFetchData(data);
 
-  console.log('items from GetData:', items);
-
-  return (
-    <>
-      {items &&
-        items.map(item => (
-          <>
-            {pageContext === 'quotes' ? (
-              <QuoteCard item={item} key={item.id} />
-            ) : (
-              <CardSmall item={item} key={item.id} />
-            )}
-          </>
+  const MasonryLayout = () => {
+    return (
+      <Masonry
+        breakpointCols={breakpointColumnsObj}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column"
+      >
+        {items.map(item => (
+          <QuoteCardSmall item={item} key={item.id} />
         ))}
-    </>
-  );
+      </Masonry>
+    );
+  };
+
+  const OtherPages = () => {
+    return items.map(item => <CardSmall item={item} key={item.id} />);
+  };
+
+  return <>{pageContext === 'quotes' ? <MasonryLayout /> : <OtherPages />}</>;
 };
 
 GetData.propTypes = {
