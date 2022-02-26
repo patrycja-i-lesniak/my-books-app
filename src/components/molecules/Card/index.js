ea067fb0a4544b58f6a59481ae8998d8d667a5b8
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-02-27 00:12:19 +0100

    add export default from QuoteCardSmall to index file

--- src/components/molecules/Card/index.js
+++ src/components/molecules/Card/index.js
@@ -1,3 +1,3 @@
 export { default as CardBig } from './CardBig';
 export { default as CardSmall } from './CardSmall';
-export { default as QuoteCard } from './QuoteCard';
+export { default as QuoteCardSmall } from './QuoteCardSmall';
