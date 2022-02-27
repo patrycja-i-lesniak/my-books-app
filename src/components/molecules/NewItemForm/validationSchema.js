3149fbf5814509ea95b5eaa6acec58fd4839d84a
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-02-27 17:43:55 +0100

    add quote validation schema

--- src/components/molecules/NewItemForm/validationSchema.js
+++ src/components/molecules/NewItemForm/validationSchema.js
@@ -48,3 +48,10 @@ export const noteValidationSchema = Yup.object().shape({
   date: Yup.date().nullable().notRequired(),
   content: Yup.string().required('Required field is empty'),
 });
+
+export const quoteValidationSchema = Yup.object().shape({
+  quote: Yup.string().required('Required field is empty'),
+  author: Yup.string().min(2, 'Too Short!'),
+  title: Yup.string().min(2, 'Too Short!').max(50, 'Too Long!'),
+  checked: Yup.boolean().oneOf([true, false]),
+});
