ee6abdf36388640d88fefff6f74671678bfea0a4
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-03-02 20:35:36 +0100

    moved NewItem Slider to separate file, change paths, move this component from Maintemplate to GridTemplate

--- src/templates/GridTemplate.js
+++ src/templates/GridTemplate.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import styled, { css } from 'styled-components';
 import PropTypes from 'prop-types';
 
@@ -8,6 +9,20 @@ import withContext from 'hoc/withContext';
 import RotaryBox from 'components/molecules/RotaryBox/RotaryBox';
 import QuotesBox from 'components/organisms/QuotesBox';
 import { capitalizeFirstLetter } from 'helpers/capitalizeFirstLetter';
+
+import NewItemSlider from 'components/organisms/NewItemSlider';
+import { StyledBackdrop } from 'theme/GlobalStyle';
+import ButtonIcon from 'components/atoms/ButtonIcon/ButtonIcon';
+import plusIcon from 'assets/icons/plus.svg';
+
+const StyledButtonIcon = styled(ButtonIcon)`
+  border-radius: 50%;
+  position: fixed;
+  bottom: 100px;
+  right: 20px;
+  z-index: 999;
+  box-shadow: ${({ theme }) => theme.shadows.boxShadowDark};
+`;
 
 const StyledGrid = styled.div`
   display: grid;
@@ -25,30 +40,47 @@ const StyledGrid = styled.div`
     `}
 `;
 
-const GridTemplate = ({ children, pageContext }) => (
-  <UserPageTemplate>
-    <Container>
-      {pageContext === 'home' ? (
-        <>
-          <QuotesBox />
-          <RotaryBox />
+const GridTemplate = ({ children, pageContext }) => {
+  const [isNewItemSliderVisible, setNewItemSliderVisible] = useState(false);
 
-          <Header>Last added books</Header>
-        </>
-      ) : (
-        <Header>{capitalizeFirstLetter(pageContext)}</Header>
-      )}
+  const toggleNewItemSlider = () => {
+    setNewItemSliderVisible(!isNewItemSliderVisible);
+  };
 
-      {pageContext === 'notes' ? (
-        <StyledGrid notes>{children}</StyledGrid>
-      ) : pageContext === 'quotes' ? (
-        <div>{children}</div>
-      ) : (
-        <StyledGrid>{children}</StyledGrid>
-      )}
-    </Container>
-  </UserPageTemplate>
-);
+  return (
+    <UserPageTemplate>
+      <Container>
+        {pageContext === 'home' ? (
+          <>
+            <QuotesBox />
+            <RotaryBox />
+
+            <Header>Last added books</Header>
+          </>
+        ) : (
+          <Header>{capitalizeFirstLetter(pageContext)}</Header>
+        )}
+
+        {pageContext === 'notes' ? (
+          <StyledGrid notes>{children}</StyledGrid>
+        ) : pageContext === 'quotes' ? (
+          <div>{children}</div>
+        ) : (
+          <StyledGrid>{children}</StyledGrid>
+        )}
+
+        <NewItemSlider
+          isVisible={isNewItemSliderVisible}
+          toggleNewItemSlider={toggleNewItemSlider}
+        />
+        {pageContext === 'home' || isNewItemSliderVisible ? null : (
+          <StyledButtonIcon icon={plusIcon} onClick={toggleNewItemSlider} />
+        )}
+        {isNewItemSliderVisible && <StyledBackdrop onClick={toggleNewItemSlider} />}
+      </Container>
+    </UserPageTemplate>
+  );
+};
 
 GridTemplate.propTypes = {
   children: PropTypes.node,
--- src/templates/MainTemplate.js
+++ src/templates/MainTemplate.js
@@ -3,45 +3,21 @@ import styled, { ThemeProvider } from 'styled-components';
 import PropTypes from 'prop-types';
 import { useLocation } from 'react-router';
 
-import plusIcon from 'assets/icons/plus.svg';
 import PageContext from 'context';
 import ButtonIcon from 'components/atoms/ButtonIcon/ButtonIcon';
-import NewItemSlider from 'components/organisms/Sliders/NewItemSlider';
-import { GlobalStyle, StyledBackdrop } from 'theme/GlobalStyle';
+import { GlobalStyle } from 'theme/GlobalStyle';
 import { theme } from 'theme/theme';
 
 import { useGetCurrentPage } from 'customHooks';
 
-const StyledButtonIcon = styled(ButtonIcon)`
-  border-radius: 50%;
-  position: fixed;
-  bottom: 100px;
-  right: 20px;
-  z-index: 999;
-  box-shadow: ${({ theme }) => theme.shadows.boxShadowDark};
-`;
-
 const MainTemplate = ({ children }) => {
-  const [isNewItemSliderVisible, setNewItemSliderVisible] = useState(false);
-
   const pageType = useGetCurrentPage();
-
-  const toggleNewItemSlider = () => {
-    setNewItemSliderVisible(!isNewItemSliderVisible);
-  };
 
   return (
     <div>
       <PageContext.Provider value={pageType}>
         <GlobalStyle />
-        <ThemeProvider theme={theme}>
-          {children}
-          <NewItemSlider isVisible={isNewItemSliderVisible} toggleNewItemSlider={toggleNewItemSlider} />
-          {pageType === 'home' || isNewItemSliderVisible ? null : (
-            <StyledButtonIcon icon={plusIcon} onClick={toggleNewItemSlider} />
-          )}
-          {isNewItemSliderVisible && <StyledBackdrop onClick={toggleNewItemSlider} />}
-        </ThemeProvider>
+        <ThemeProvider theme={theme}>{children}</ThemeProvider>
       </PageContext.Provider>
     </div>
   );
