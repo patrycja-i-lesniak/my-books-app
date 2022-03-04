e44adee113f9b7cbdca18b4e55337b5aa2f07614
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-03-04 17:00:02 +0100

    add default imports to index file

---
+++ src/components/molecules/Popups/index.js
@@ -0,0 +1,4 @@
+export { default as ConfirmationPopup } from './ConfirmationPopup';
+export { default as EditPopup } from './EditPopup';
+export { default as ErrorPopup } from './ErrorPopup';
+export { default as SuccessPopup } from './SuccessPopup';
