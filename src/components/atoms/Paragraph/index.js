6a19458beffa015cd33aad01a3917f2ef1183813
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-03-04 16:54:12 +0100

    change color of paragraph

--- src/components/atoms/Paragraph/index.js
+++ src/components/atoms/Paragraph/index.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const Paragraph = styled.p`
   font-size: 1.4rem;
   line-height: 1.5;
-  color: ${({ theme }) => theme.colors.black};
+  color: ${({ theme }) => theme.colors.grey};
 `;
 
 export default Paragraph;
