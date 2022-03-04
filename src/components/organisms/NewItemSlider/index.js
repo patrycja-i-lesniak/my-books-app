76027610b6b0f38565302a090a94a15e2fb00e07
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-03-04 17:27:17 +0100

    get id, change text of the paragraph on the page, format code

--- src/components/organisms/NewItemSlider/index.js
+++ src/components/organisms/NewItemSlider/index.js
@@ -1,24 +1,41 @@
 import { BsCaretRightFill } from 'react-icons/bs';
 import PropTypes from 'prop-types';
+import { useParams } from 'react-router-dom';
 import { StyledWrapper, StyledHeader, StyledParagraph, CloseSliderButton } from './styled';
 import NewItemForm from 'components/molecules/Forms/NewItemForm';
 import withContext from 'hoc/withContext';
 
-const NewItemSlider = ({ isVisible, pageContext, toggleNewItemSlider }) => (
-  <>
+const NewItemSlider = ({ isVisible, pageContext, toggleNewItemSlider }) => {
+  const { id } = useParams();
+
+  return (
     <StyledWrapper isVisible={isVisible}>
-      <StyledHeader secondary>Add new {pageContext}</StyledHeader>
-      <StyledParagraph>Fill in all the fields and click the button.</StyledParagraph>
+      {id ? (
+        <>
+          <StyledHeader secondary>Edit {pageContext && pageContext.slice(0, -1)}</StyledHeader>
+          <StyledParagraph>
+            Load saved data or fill in all fields of the form and click the edit button.
+          </StyledParagraph>
+        </>
+      ) : (
+        <>
+          <StyledHeader secondary>Add new {pageContext && pageContext.slice(0, -1)}</StyledHeader>
+          <StyledParagraph>
+            Fill in all fields of the form and click the add button.
+          </StyledParagraph>
+        </>
+      )}
       <NewItemForm toggleNewItemSlider={toggleNewItemSlider} />
       <CloseSliderButton type="button" onClick={toggleNewItemSlider}>
         <BsCaretRightFill />
       </CloseSliderButton>
     </StyledWrapper>
-  </>
-);
+  );
+};
 
 NewItemSlider.propTypes = {
   pageContext: PropTypes.oneOf(['home', 'books', 'authors', 'notes', 'quotes']),
+  id: PropTypes.string,
   isVisible: PropTypes.bool,
   toggleNewItemSlider: PropTypes.func,
 };
