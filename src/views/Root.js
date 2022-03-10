48c5a6478d13ca4cf5358a759a6ba023097d3aa7
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-03-10 21:51:49 +0100

    format code

--- src/views/Root.js
+++ src/views/Root.js
@@ -10,21 +10,21 @@ import DetailsPage from 'views/DetailsPage';
 import { routes } from 'routes';
 
 const Root = () => (
-    <HashRouter>
-      <MainTemplate>
-        <Switch>
-          <Route exact path="/" render={() => <Redirect to={routes.home} />} />
-          <Route exact path={routes.home} component={Home} />
-          <Route exact path={routes.books} component={Books} />
-          <Route path={routes.book} component={DetailsPage} />
-          <Route exact path={routes.authors} component={Authors} />
-          <Route path={routes.author} component={DetailsPage} />
-          <Route exact path={routes.notes} component={Notes} />
-          <Route path={routes.note} component={DetailsPage} />
-          <Route exact path={routes.quotes} component={Quotes} />
-        </Switch>
-      </MainTemplate>
-    </HashRouter>
+  <HashRouter>
+    <MainTemplate>
+      <Switch>
+        <Route exact path="/" render={() => <Redirect to={routes.home} />} />
+        <Route exact path={routes.home} component={Home} />
+        <Route exact path={routes.books} component={Books} />
+        <Route path={routes.book} component={DetailsPage} />
+        <Route exact path={routes.authors} component={Authors} />
+        <Route path={routes.author} component={DetailsPage} />
+        <Route exact path={routes.notes} component={Notes} />
+        <Route path={routes.note} component={DetailsPage} />
+        <Route exact path={routes.quotes} component={Quotes} />
+      </Switch>
+    </MainTemplate>
+  </HashRouter>
 );
 
 export default Root;
