9bfe5e2dec2cf611a46e0cbe64c2f62b5426804e
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-03-10 21:50:12 +0100

    change hook to async function, unsuscribe setItems and setItemsData

--- src/customHooks/useFetchData.js
+++ src/customHooks/useFetchData.js
@@ -2,30 +2,30 @@ import React, { useState, useEffect } from 'react';
 import { base } from 'airtable/base';
 
 const useFetchData = data => {
-  const [items, setItems] = useState({ status: 'loading', items: null });
+  const [items, setItems] = useState({ status: 'loading', items: {} });
   const { table, pageSize, sort, filterByFormula } = data;
 
   useEffect(() => {
-    // let isBaseSubscribed = true;
-    // if (isBaseSubscribed) {
-    base(`${table}`)
-      .select({
-        view: 'Grid view',
-        pageSize: pageSize,
-        sort: sort,
-        filterByFormula: filterByFormula,
-      })
-      .firstPage(function (err, records) {
-        if (err) {
-          console.error(err);
-          return;
-        }
-        setItems(records);
-      });
-    // }
-    // return () => {
-    //  isBaseSubscribed = false;
-    // };
+    const getData = async () => {
+      const records = await base(`${table}`)
+        .select({
+          view: 'Grid view',
+          pageSize: pageSize,
+          sort: sort,
+          filterByFormula: filterByFormula,
+        })
+        .firstPage(function (err, records) {
+          if (err) {
+            console.error(err);
+            return;
+          }
+          setItems(records);
+        });
+    };
+    getData();
+    return () => {
+      setItems();
+    };
   }, [pageSize, sort, table, filterByFormula]);
 
   return {
--- src/customHooks/useFetchDetailsData.js
+++ src/customHooks/useFetchDetailsData.js
@@ -5,18 +5,24 @@ const useFetchDetailsData = id => {
   const [itemData, setItemData] = useState({ status: 'loading', data: null });
 
   useEffect(() => {
-    base('books').find(`${id}`, function (err, record) {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      setItemData(record);
-    });
+    const getItemById = async id => {
+      const record = await base('books').find(`${id}`, function (err, record) {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        setItemData(record);
+      });
+    };
+    getItemById(id);
+    return () => {
+      setItemData();
+    };
   }, [id]);
+
   return {
-    itemData
+    itemData,
   };
 };
 
 export default useFetchDetailsData;
-
