948b165bdd28a223aa6c9fbae26283a14c8f4cee
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-03-10 21:46:14 +0100

    destructure location

--- src/customHooks/useGetCurrentPage.js
+++ src/customHooks/useGetCurrentPage.js
@@ -3,12 +3,12 @@ import { useLocation } from 'react-router';
 
 const useGetCurrentPage = () => {
   const [pageType, setPageType] = useState();
-  const location = useLocation();
+  const { pathname } = useLocation();
 
   const setCurrentPage = () => {
     const pageTypes = ['home', 'books', 'authors', 'notes', 'quotes'];
 
-    const [currentPage] = pageTypes.filter(page => location.pathname.includes(page));
+    const [currentPage] = pageTypes.filter(page => pathname.includes(page));
     if (pageType !== currentPage) {
       setPageType(currentPage);
     }
