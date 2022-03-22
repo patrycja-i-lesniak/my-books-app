59393c8fe6ae15c56b891855eb3c33f3d55b5c2f
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-03-22 13:41:54 +0100

    format code

--- src/views/Quotes.js
+++ src/views/Quotes.js
@@ -8,41 +8,38 @@ import LoaderSmall from 'components/atoms/LoaderSmall';
 import Error from 'components/atoms/Error';
 
 const Quotes = () => {
-  const data = {
-    table: 'quotes',
-    pageSize: 100,
-    sort: [{ field: 'author', direction: 'asc' }],
-    filterByFormula: '',
-  };
+	const data = {
+		table: 'quotes',
+		pageSize: 100,
+		sort: [ { field: 'author', direction: 'asc' } ],
+		filterByFormula: ''
+	};
 
-  const breakpointColumnsObj = {
-    default: 3,
-    850: 2,
-    500: 1,
-  };
+	const breakpointColumnsObj = {
+		default: 3,
+		850: 2,
+		500: 1
+	};
 
-  const { items } = useFetchData(data);
+	const { items } = useFetchData(data);
 
-  return (
-    <GridTemplate pageType="quotes">
-      {items.status === 'loading' ? (
-        <LoaderSmall />
-      ) : items.status === 'error' ? (
-        <Error reloadButton />
-      ) :  (
-        <>
-      <Masonry
-        breakpointCols={breakpointColumnsObj}
-        className="my-masonry-grid"
-        columnClassName="my-masonry-grid_column"
-      >
-        {items.map(item => (
-          <QuoteCardSmall item={item} key={item.id} />
-        ))}
-      </Masonry>
-      </> )}
-    </GridTemplate>
-  );
+	return (
+		<GridTemplate pageType="quotes">
+			{items.status === 'loading' ? (
+				<LoaderSmall />
+			) : items.status === 'error' ? (
+				<Error reloadButton />
+			) : (
+				<Masonry
+					breakpointCols={breakpointColumnsObj}
+					className="my-masonry-grid"
+					columnClassName="my-masonry-grid_column"
+				>
+					{items.map((item) => <QuoteCardSmall item={item} key={item.id} />)}
+				</Masonry>
+			)}
+		</GridTemplate>
+	);
 };
 
 export default Quotes;
