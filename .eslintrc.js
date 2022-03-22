cbe38de1070e7ebe88ba98235a6f1d8ef969d67c
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2022-03-22 14:11:35 +0100

    delete unnecessary config file, install packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,41 @@
 /* eslint-disable no-undef */
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  extends: ['plugin:react/recommended', 'plugin:react-hooks/recommended', 
-  //'eslint:recommended'
-],
+	env: {
+		browser: true,
+		es2021: true,
+		node: true
+	},
+	extends: [
+		'plugin:react/recommended',
+		'plugin:react-hooks/recommended'
+		//'eslint:recommended'
+	],
 
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 12,
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true
+		},
+		ecmaVersion: 12,
 
-    sourceType: 'module',
-  },
-  globals: {
-    MyGlobal: true,
-    window: true,
-    document: true,
-  },
-  plugins: ['react', 'import', 'react-hooks'],
-  rules: {
-    // 'import/named': 1,
-    // 'import/namespace': 1,
-    // 'import/default': 1,
-    // 'import/export': 1,
-    'react/react-in-jsx-scope': 'off',
-    // 'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-    'react/prop-types': 1,
-    // 'react-hooks/rules-of-hooks': 'error',
-    // 'react-hooks/exhaustive-deps': 'warn',
-    // 'import/prefer-default-export': 0,
-    // 'import/no-extraneous-dependencies': 0,
-  },
+		sourceType: 'module'
+	},
+	globals: {
+		MyGlobal: true,
+		window: true,
+		document: true
+	},
+	plugins: [ 'react', 'import', 'react-hooks' ],
+	rules: {
+		// 'import/named': 1,
+		// 'import/namespace': 1,
+		// 'import/default': 1,
+		// 'import/export': 1,
+		'react/react-in-jsx-scope': 'off',
+		// 'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+		'react/prop-types': 1
+		// 'react-hooks/rules-of-hooks': 'error',
+		// 'react-hooks/exhaustive-deps': 'warn',
+		// 'import/prefer-default-export': 0,
+		// 'import/no-extraneous-dependencies': 0,
+	}
 };
